f9f6a1b1bdc57de6fd34c188f055b5f90a553406
soun997 <74900921+soun997@users.noreply.github.com>
2023-01-18 15:42:23 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 124 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb1 = new StringBuilder();
+		StringBuilder sb2 = new StringBuilder();
+		int N = Integer.parseInt(br.readLine());
+		for (int i = 0; i < N * 2 - 1; i++) {
+			sb1.append("*");
+		}
+		for (int i = 0; i < N; i++) {
+			if (i == 0) {
+				sb2.append(sb1).append("\n");
+				sb1.delete(0,  2);
+				continue;
+			}
+			sb2.append(" ").append(sb1).append("\n");
+			sb1.insert(0, " ");
+			sb1.delete(sb1.length() - 2, sb1.length());
+		}
+		System.out.println(sb2.toString());
+	}
+}
