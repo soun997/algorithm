b3429caf9b4edf02eb24dcf8a1f83ae0e0421f4d
soun997 <74900921+soun997@users.noreply.github.com>
2023-01-22 02:03:30 +0900

    [Bronze IV] Title: 기찍 N, Time: 304 ms, Memory: 20420 KB -BaekjoonHub

--- 백준/Bronze/2742. 기찍 N/기찍 N.java
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -5,10 +5,12 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
         int n = sc.nextInt();
 
         for (int i = n; i > 0; i--){
-            System.out.println(i);
+            sb.append(i).append("\n");
         }
+        System.out.println(sb);
     }
 }
