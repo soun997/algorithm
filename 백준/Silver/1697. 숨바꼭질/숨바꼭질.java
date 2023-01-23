41fe21b400bc0eca91a080c6bfbb029a09b97c44
soun997 <74900921+soun997@users.noreply.github.com>
2023-01-24 03:11:57 +0900

    [Silver I] Title: 숨바꼭질, Time: 256 ms, Memory: 21828 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -4,7 +4,7 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
-    
+
     public static final int MAX = 100000;
 
     public static void main(String[] args) throws Exception {
@@ -19,14 +19,30 @@ public class Main {
         q.add(n);
         while(line[k] == -1){
             int cur = q.poll();
+            int n1 = cur - 1;
+            int n2 = cur + 1;
+            int n3 = 2 * cur;
 
-            for (int next : new int[]{cur - 1, cur + 1, 2 * cur}){
-                if (next < 0 || next > MAX) continue;
-                if (line[next] != -1) continue;
-                line[next] = line[cur] + 1;
-                q.add(next);
+            if (!(n1 < 0) && !(n1 > MAX)){
+                if (line[n1] == -1){
+                    line[n1] = line[cur] + 1;
+                    q.add(n1);
+                }
             }
 
+            if (!(n2 < 0) && !(n2 > MAX)){
+                if (line[n2] == -1){
+                    line[n2] = line[cur] + 1;
+                    q.add(n2);
+                }
+            }
+
+            if (!(n3 < 0) && !(n3 > MAX)){
+                if (line[n3] == -1){
+                    line[n3] = line[cur] + 1;
+                    q.add(n3);
+                }
+            }
         }
         System.out.println(line[k]);
     }
