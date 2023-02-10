cb61c959fab7efeeab56cc747930acc5f000259f
소윤파크 <74900921+soun997@users.noreply.github.com>
2023-02-10 16:26:49 +0900

    [Silver I] Title: 미로 탐색, Time: 88 ms, Memory: 12124 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,27 +1,16 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-class Pair {
-
-    public int x;
-    public int y;
-
-    public Pair(int x, int y){
-        this.x = x;
-        this.y = y;
-    }
-}
-
 public class Main {
 
-    static int[][] board = new int[102][102];
-    static int n;
-    static int m;
-    static int[] dx = { 1, 0, -1, 0 };
-    static int[] dy = { 0, 1, 0, -1 };
+    static int n, m;
+    static int[][] maze;
+    static boolean[][] visited;
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
 
     public static void main(String[] args) throws Exception {
 
@@ -29,38 +18,42 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-
+        maze = new int[n][m];
+        visited = new boolean[n][m];
         for (int i = 0; i < n; i++) {
-            String[] input = br.readLine().split("");
+            char[] input = br.readLine().toCharArray();
             for (int j = 0; j < m; j++) {
-                board[i][j] = Integer.parseInt(input[j]);
-                if (Integer.parseInt(input[j]) == 1) board[i][j] = -1;
+                maze[i][j] = input[j] - '0';
             }
         }
 
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(0, 0));
-        board[0][0] = 1;
-        int result = bfs(q);
-        System.out.println(result);
+        System.out.println(bfs(0, 0));
     }
 
-    static int bfs(Queue<Pair> q) {
-        int past = 0;
+    static int bfs(int x, int y){
+        Queue<int[]> q = new ArrayDeque<>();
+        q.offer(new int[]{x, y, 1});
+        visited[x][y] = true;
         while(!q.isEmpty()){
-            Pair cur = q.poll();
-            past = board[cur.x][cur.y]; // 전 칸의 값을 가져온다.
-            // 사방 탐색
-            for (int i = 0; i < 4; i++){
-                int nx = cur.x + dx[i];
-                int ny = cur.y + dy[i];
-                if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-                if (board[nx][ny] != -1) continue;
-                board[nx][ny] = past + 1;
-                q.add(new Pair(nx, ny));
+            int[] cur = q.poll();
+            //System.out.println(cur[0] + " " + cur[1]);
+            if (cur[0] == n - 1 && cur[1] == m - 1) return cur[2];
+            for (int i = 0; i < 4; i++) {
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
+                if (!check(nx, ny) || visited[nx][ny] || maze[nx][ny] == 0) continue;
+
+                q.offer(new int[]{nx, ny, cur[2] + 1});
+                visited[nx][ny] = true;
             }
         }
-        return board[n - 1][m - 1];
+        return -1;  // 비정상 종료 - 있을 수 없는 케이스
+    }
+
+    static boolean check(int x, int y){
+        if (x < 0 || x >= n || y < 0 || y >= m) {
+            return false;
+        }
+        return true;
     }
 }
-
