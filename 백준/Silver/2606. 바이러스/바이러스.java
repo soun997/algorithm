9eaeb1e3f2b01a971763eb296792bc5a9d9402f4
소윤파크 <74900921+soun997@users.noreply.github.com>
2023-02-13 15:11:16 +0900

    [Silver III] Title: 바이러스, Time: 80 ms, Memory: 11624 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,49 +1,44 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Set;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int n = Integer.parseInt(br.readLine());
-		int m = Integer.parseInt(br.readLine());
-		
-		ArrayList<ArrayList<Integer>> list = new ArrayList<>();
-		for (int i = 0; i < n + 1; i++) list.add(new ArrayList<>());
-		
-		for (int i = 0; i < m; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int v1 = Integer.parseInt(st.nextToken());
-			int v2 = Integer.parseInt(st.nextToken());
-			list.get(v1).add(v2);
-			list.get(v2).add(v1);
-		}
-		
-		int head = 1;
-		Queue<Integer> q = new LinkedList<>();
-		q.add(head);
-		boolean[] infected = new boolean[n+1];
-		infected[1] = true;
-		int cnt = 0;
-		while (!q.isEmpty()) {
-			head = q.poll();
-			for (int i = 0; i < list.get(head).size(); i++) {
-				int computer = list.get(head).get(i);
-				if (!infected[computer]) {
-					q.add(computer);
-					infected[computer] = true;
-					cnt++;
-				}
-			}
-		}
-		System.out.println(cnt);
-	}
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+        for (int i = 0; i < n + 1; i++) list.add(new ArrayList<>());
+
+        for (int i = 0; i < m; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int v1 = Integer.parseInt(st.nextToken());
+            int v2 = Integer.parseInt(st.nextToken());
+            list.get(v1).add(v2);
+            list.get(v2).add(v1);
+        }
+
+        int head = 1;
+        Stack<Integer> stk = new Stack<>();
+        stk.add(head);
+        boolean[] infected = new boolean[n+1];
+        infected[head] = true;
+        int cnt = 0;
+        while (!stk.isEmpty()) {
+            head = stk.pop();
+            for (int i = 0; i < list.get(head).size(); i++) {
+                int computer = list.get(head).get(i);
+                if (!infected[computer]) {
+                    stk.add(computer);
+                    infected[computer] = true;
+                    cnt++;
+                }
+            }
+        }
+        System.out.println(cnt);
+    }
 }
