d9f3d3aa04a7d0c4adea86d16b699faab70e1d3e
소윤파크 <74900921+soun997@users.noreply.github.com>
2023-02-24 14:38:10 +0900

    [Gold IV] Title: 알파벳, Time: 952 ms, Memory: 12452 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,73 +1,56 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Stack;
-
+import java.util.*;
 public class Main {
 
+    static char[][] board;
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, -1, 0, 1};
     static int r;
     static int c;
-    static char[][] board;
-    static int[] alphabets = new int[26];
-
+    static boolean[] visited;
     static int max = Integer.MIN_VALUE;
-
 
     public static void main(String[] args) throws Exception {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] init = br.readLine().split(" ");
-        r = Integer.parseInt(init[0]);
-        c = Integer.parseInt(init[1]);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        r = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
         board = new char[r][c];
-
+        visited = new boolean[26];
         for (int i = 0; i < r; i++) {
             char[] input = br.readLine().toCharArray();
             for (int j = 0; j < c; j++) {
                 board[i][j] = input[j];
             }
         }
-        System.out.println(dfs(0, 0, 1));
+        visited[board[0][0] % 65] = true;
+        dfs(0, 0, 1);
+        visited[board[0][0] % 65] = false;
+        System.out.println(max);
     }
 
-    static int dfs(int x, int y, int d){
-        Stack<Pair> stk = new Stack<>();
-        alphabets[board[x][y] % 65]++;
-        stk.push(new Pair(x, y, d, Arrays.copyOf(alphabets, 26)));
-        while (!stk.empty()) {
-            Pair cur = stk.pop();
-            boolean movable = false;
-            //System.out.println(board[cur.x][cur.y] + " " + cur.distance);
-            for (int i = 0; i < 4; i++) {
-                int nx = cur.x + dx[i];
-                int ny = cur.y + dy[i];
-                if (nx < 0 || nx >= r || ny < 0 || ny >= c) continue;
-                if (cur.status[board[nx][ny] % 65] > 0) continue;
-                movable = true;
-                int[] status = Arrays.copyOf(cur.status, 26);
-                status[board[nx][ny] % 65]++;
-                stk.push(new Pair(nx, ny, cur.distance + 1, status));
+    static void dfs(int x, int y, int cnt) {
+
+        for (int d = 0; d < 4; d++) {
+            int nx = x + dx[d];
+            int ny = y + dy[d];
+            if (!check(nx, ny) || visited[(board[nx][ny] % 65)]){
+                continue;
             }
-            if (!movable){
-                max = Math.max(max, cur.distance);
-            }
+            visited[board[nx][ny] % 65] = true;
+            dfs(nx, ny, cnt + 1);
+            visited[board[nx][ny] % 65] = false;
         }
-        return max;
+
+        max = Math.max(max, cnt);
     }
 
-    static class Pair {
-        public int x;
-        public int y;
-        public int distance;
-        public int[] status;
-
-        public Pair(int x, int y, int distance, int[] status){
-            this.x = x;
-            this.y = y;
-            this.distance = distance;
-            this.status = status;
+    static boolean check(int x, int y){
+        if (x < 0 || x >= r || y < 0 || y >= c){
+            return false;
         }
+        return true;
     }
 }
