958e68b689a788e0fa7db38ea4dd52291f327c59
소윤파크 <74900921+soun997@users.noreply.github.com>
2023-02-27 03:30:26 +0900

    [Gold III] Title: 사회망 서비스(SNS), Time: 1536 ms, Memory: 362628 KB -BaekjoonHub

--- 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.java
+++ 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.java
@@ -2,58 +2,64 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static LinkedList<Integer>[] adj;
-	public static boolean[] visited;
-	public static int[][] dp; //[][0]: 해당노드가 E가 아닐 때, [][1]: 해당노드가 E일 때
-	public static int N;
-	public static int answer = 0;
 
-	public static void main(String[] args) throws Exception{
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
-		
-		//입력 및 초기화
-		N = read();
-		adj = new LinkedList[N+1];
-		visited = new boolean[N+1];
-		dp = new int[N+1][2];
+    static class Node {
+        int root;
+        Node child;
 
-		for (int i=1; i<N+1; i++) {
-			adj[i] = new LinkedList<Integer>();
-		}
+        public Node(int root) {
+            this.root = root;
+        }
 
-		for(int i=0; i<N-1; i++) {
-			int num1 = read();
-			int num2 = read();
-			adj[num1].add(num2);
-			adj[num2].add(num1);
-		}
+        public Node(int root, Node child) {
+            this.root = root;
+            this.child = child;
+        }
+    }
 
-		dp(1, -1); //루트 1설정
-		
-		sb.append(Math.min(dp[1][0], dp[1][1]));
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-	}
+    static Node[] nodes;
+    static int count;
 
-	static void dp(int cur, int parent) {
-		dp[cur][0] = 0;
-		dp[cur][1] = 1;
-		
-		for(int child : adj[cur]) {
-			if(child != parent) { 
-				dp(child, cur); 
-				dp[cur][0] += dp[child][1]; //자신이 E가 아니면 자식은 무조건 E여야 한다.
-				dp[cur][1] += Math.min(dp[child][0], dp[child][1]); //자신이 E이면 자식은 상관없다. (최소를 뽑는다.)
-			}
-		}
-	}
-    
-    static int read() throws Exception {
-        int c, n = System.in.read() & 15;
-        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
-        return n;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+
+        nodes = new Node[N + 1];
+        for (int i = 1; i <= N; i++) {
+            nodes[i] = new Node(i);
+        }
+
+        for (int i = 1; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+
+            nodes[s].child = new Node(e, nodes[s].child);
+            nodes[e].child = new Node(s, nodes[e].child);
+        }
+        
+        dfs(1, 0);
+        System.out.println(count);
+    }
+
+    private static boolean dfs(int n, int prev) { // leaf 노드랑 연결되어있으면 사용, 사용된 노드는 가지치기
+        boolean isLeaf = true;
+
+        for (Node node = nodes[n].child; node != null; node = node.child) {
+            if (node.root == prev) {
+                continue;
+            }
+
+            if (dfs(node.root, n)) { // leaf 노드를 갖고 있는가?
+                isLeaf = false;
+            }
+        }
+
+        if (!isLeaf) {
+            count++;
+        }
+        return isLeaf;
     }
 
 }
