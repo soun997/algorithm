8dd1faaa2e293d55063d94d3e16ceddaec303138
소윤파크 <74900921+soun997@users.noreply.github.com>
2023-03-03 16:42:07 +0900

    [Gold III] Title: 아기 상어, Time: 92 ms, Memory: 12292 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.java
+++ 백준/Gold/16236. 아기 상어/아기 상어.java
@@ -5,100 +5,124 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int n;
+    static class Fish implements Comparable<Fish> {
+        public int x;
+        public int y;
+        public int distance = 0;
+
+        public Fish(int x, int y, int distance) {
+            this.x = x;
+            this.y = y;
+            this.distance = distance;
+        }
+
+        @Override
+        public int compareTo(Fish other) {
+            if (this.distance < other.distance) {
+                return -1;
+            } else if (this.distance == other.distance) {
+                // 더 위에 있는 먹이 우선
+                if (this.x < other.x) {
+                    return -1;
+                }
+                // 더 왼쪽에 있는 먹이 우선
+                else if (this.x == other.x) {
+                    return Integer.compare(this.y, other.y);
+                } else {
+                    return 1;
+                }
+            } else {
+                return 1;
+            }
+
+        }
+    }
+    static int N;
     static int[][] sea;
-    static boolean[][] check;
-    static int[] dx = {-1, 0, 0, 1,};
-    static int[] dy = {0, -1, 1, 0};
-    static int sec = 0;
+    static boolean[][] visited;
+    static int time = 0;
     static int size = 2;
-    static int r;
-    static int c;
     static int eaten = 0;
+    static int[] start;     // 상어의 시작위치
+    static int[] dx = {-1, 0, 0, 1};
+    static int[] dy = {0, -1, 1, 0};
 
     public static void main(String[] args) throws Exception {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        sea = new int[n][n];
-        for (int i = 0; i < n; i++) {
+        N = Integer.parseInt(br.readLine());
+        sea = new int[N][N];
+
+        start = new int[2];
+        for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < N; j++) {
                 sea[i][j] = Integer.parseInt(st.nextToken());
+                // 악어의 위치 초기화
                 if (sea[i][j] == 9) {
-                    r = i;
-                    c = j;
+                    start[0] = i;
+                    start[1] = j;
                     sea[i][j] = 0;
                 }
             }
         }
-        while(hunt()) {}
-        System.out.println(sec);
+        while (true){
+            if (!hunt()){
+                break;
+            }
+        }
+        System.out.println(time);
     }
 
     static boolean hunt() {
-        check = new boolean[n][n];
-        check[r][c] = true;
-        PriorityQueue<Pair> q = new PriorityQueue<Pair>();
-        q.offer(new Pair(r, c, 0));
+
+        visited = new boolean[N][N];
+        visited[start[0]][start[1]] = true;
+
+        PriorityQueue<Fish> q = new PriorityQueue<>();
+        q.offer(new Fish(start[0], start[1], 0));
         while(!q.isEmpty()) {
-            Pair cur = q.poll();
+            Fish cur = q.poll();
+            // 현재 상어가 고기를 잡아먹을 수 있는 상태 (우선순위 큐에서 꺼냈으므로 가장 최적의 값)
             if (sea[cur.x][cur.y] < size && sea[cur.x][cur.y] != 0) {
                 sea[cur.x][cur.y] = 0;
-                sec = sec + cur.distance;
+                time = time + cur.distance;
                 eaten++;
                 if (eaten == size){
                     size++;
                     eaten = 0;
                 }
-                r = cur.x;
-                c = cur.y;
+                // 다음 bfs 의 시작위치
+                start[0] = cur.x;
+                start[1] = cur.y;
                 return true;
             }
+            // 아니라면 다음 위치로 이동
             for (int i = 0; i < 4; i++) {
                 int nx = cur.x + dx[i];
                 int ny = cur.y + dy[i];
-                if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
-                if (check[nx][ny]) continue;
-                if (sea[nx][ny] > size) continue;
-                q.offer(new Pair(nx, ny, cur.distance + 1));
-                check[nx][ny] = true;
+                if (!check(nx, ny)) {
+                    continue;
+                }
+                if (visited[nx][ny]) {
+                    continue;
+                }
+                if (sea[nx][ny] > size) {
+                    continue;
+                }
+                q.offer(new Fish(nx, ny, cur.distance + 1));
+                visited[nx][ny] = true;
             }
 
         }
 
         return false;
     }
-}
 
-class Pair implements Comparable<Pair> {
-    public int x;
-    public int y;
-    public int distance = 0;
-    public Pair(int x, int y, int distance) {
-        this.x = x;
-        this.y = y;
-        this.distance = distance;
-    }
-
-    @Override
-    public int compareTo(Pair target) {
-        if (this.distance < target.distance){
-            return -1;
+    static boolean check(int x, int y){
+        if (x < 0 || x >= N || y < 0 || y >= N){
+            return false;
         }
-        else if (this.distance == target.distance){
-            // 더 위에 있는 먹이 우선
-            if (this.x < target.x) {
-                return -1;
-            }
-            // 더 왼쪽에 있는 먹이 우선
-            else if (this.x == target.x) {
-                if (this.y < target.y) {
-                    return -1;
-                }
-            }
-        }
-        return 1;
+        return true;
     }
 }
-
