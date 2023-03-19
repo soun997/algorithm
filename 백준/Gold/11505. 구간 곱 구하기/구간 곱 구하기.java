927886f766d3d01b0c5fd29061898ad25e796604
소윤파크 <soun997@gmail.com>
2023-03-19 22:30:13 +0900

    [Gold I] Title: 구간 곱 구하기, Time: 484 ms, Memory: 116388 KB -BaekjoonHub

---
+++ 백준/Gold/11505. 구간 곱 구하기/구간 곱 구하기.java
@@ -0,0 +1,130 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static class SegmentTree {
+
+        final long DIV = 1_000_000_007;
+
+        int size;
+        long[] tree;
+
+        // 주어진 배열로 Segment Tree 를 생성
+        public SegmentTree(long[] arr, int size) {
+            build(arr, size);
+        }
+
+        private long merge(long left, long right) {
+            // sum 을 구하고 싶은 경우
+            return (left * right) % DIV;    // min(left, right) 혹은 max(left, right) 등으로도 사용 가능
+        }
+
+        public void build(long[] arr, int size) {
+            this.size = size;
+            this.tree = new long[size * 4];
+
+            buildRec(arr, 1, 0, size - 1);
+        }
+
+        private long buildRec(long[] arr, int node, int nodeLeft, int nodeRight) {
+            // 자식노드가 하나인 경우
+            if (nodeLeft == nodeRight) {
+                return tree[node] = arr[nodeLeft];
+            }
+
+            // (nodeLeft + nodeRight) / 2 를 해도 무방하나, overflow 방지
+            int mid = nodeLeft + (nodeRight - nodeLeft) / 2;
+            long leftVal = buildRec(arr, node * 2, nodeLeft, mid);
+            long rightVal = buildRec(arr, node * 2 + 1, mid + 1, nodeRight);
+
+            return tree[node] = merge(leftVal, rightVal);
+        }
+
+        // left ~ right 까지의 연산 결과 구하기
+        public long query(int left, int right) {
+            return queryRec(left, right, 1, 0, size - 1);
+        }
+
+        private long queryRec(long left, long right, int node, int nodeLeft, int nodeRight) {
+
+            // 내가 처리하려는 구간을 벗어나는 경우
+            if (left > nodeRight || right < nodeLeft) {
+                return 1;   // default value
+            }
+
+            // 처리하려는 구간에 완전히 포함되는 경우
+            if (left <= nodeLeft && right >= nodeRight) {
+                return tree[node];
+            }
+
+            // 처리하려는 구간에 걸치는 경우 (구간에 벗어나는 애들과 포함되는 애들을 나누어서 계산해야 하기 때문)
+            int mid = nodeLeft + (nodeRight - nodeLeft) / 2;
+
+            return merge(
+                    queryRec(left, right, node * 2, nodeLeft, mid),
+                    queryRec(left, right, node * 2 + 1, mid + 1, nodeRight)
+            );
+        }
+
+        public long update(int index, long newValue){
+            return updateRec(index, newValue, 1, 0, size - 1);
+        }
+
+        private long updateRec(int index, long newValue, int node, int nodeLeft, int nodeRight){
+            // 업데이트 위치와 관련없는 segment
+            if (index < nodeLeft || index > nodeRight){
+                return tree[node];
+            }
+            // 업데이트 위치를 찾았을 경우 -> 갱신
+            if (nodeLeft == nodeRight){
+                return tree[node] = newValue;
+            }
+
+            int mid = nodeLeft + (nodeRight - nodeLeft) / 2;
+            long leftVal = updateRec(index, newValue, node * 2, nodeLeft, mid);
+            long rightVal = updateRec(index, newValue, node * 2 + 1, mid + 1, nodeRight);
+
+            return tree[node] = merge(leftVal, rightVal);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        long[] arr = new long[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        SegmentTree segmentTree = new SegmentTree(arr, N);
+
+        for (int i = 0; i < M + K; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            switch(a){
+                case 1:
+                    segmentTree.update(b - 1, c);
+                    break;
+                case 2:
+                    long result = segmentTree.query(b - 1, c - 1);
+                    sb.append(result).append("\n");
+                    break;
+                default:
+                    System.out.println("유효한 입력이 아닙니다.");;
+            }
+        }
+        System.out.println(sb);
+    }
+}
