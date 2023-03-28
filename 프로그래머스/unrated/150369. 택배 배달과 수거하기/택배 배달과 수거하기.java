11233ad81b38beaeda4fd3032458dbb311a4b852
소윤파크 <soun997@gmail.com>
2023-03-28 21:39:42 +0900

    [unrated] Title: 택배 배달과 수거하기, Time: 6.73 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/unrated/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
+++ 프로그래머스/unrated/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -1,23 +1,55 @@
 class Solution {
     
-    public long solution(int cap, int n, int[] deliveries, int[] pickups) {
-        int deliver = 0, pickup = 0;
-        long answer = 0;
+    int[] deliveries;
+    int[] pickups;
 
-        for (int i = n-1; i >= 0; i--) {
-            if(deliveries[i] != 0 || pickups[i] != 0) {
-                int cnt = 0;
-                while(deliver < deliveries[i] || pickup < pickups[i]) {
-                    cnt++;
-                    deliver += cap;
-                    pickup += cap;
-                }
-                deliver -= deliveries[i];
-                pickup -= pickups[i];
-                answer += (i+1) * cnt * 2;
-            }
+
+    int delivery(int idx, int cap, int[] houses){
+
+        if (cap < houses[idx]){
+            houses[idx] = houses[idx] - cap;
+            return 0;
         }
-        return answer;
+        cap = cap - houses[idx];
+        houses[idx] = 0;
+        return cap;
     }
 
+    int validate(int n){
+        for (int i = n - 1; i >= 0; i--){
+            if (deliveries[i] > 0 || pickups[i] > 0){
+                return (i + 1);
+            }
+        }
+        return -1;
+    }
+
+    long process(int cap, int n){
+        int deliveryCap = cap;
+        int pickupCap = cap;
+        int reached = -1;
+        long distance = 0;
+        for (int i = n - 1; i >= 0; i--){
+            if (deliveries[i] > 0 || pickups[i] > 0){
+                reached = Math.max(reached, i);
+                distance = (long)(reached + 1) * 2;
+            }
+            deliveryCap = delivery(i, deliveryCap, deliveries);
+            pickupCap = delivery(i, pickupCap, pickups);
+            if (deliveryCap == 0 && pickupCap == 0){
+                break;
+            }
+        }
+        n = validate(n);
+        if (n == -1)
+            return distance;
+        return distance + process(cap, n);
+    }
+
+    public long solution(int cap, int n, int[] deliveries, int[] pickups) {
+        this.deliveries = deliveries;
+        this.pickups = pickups;
+
+        return process(cap, n);
+    }
 }
