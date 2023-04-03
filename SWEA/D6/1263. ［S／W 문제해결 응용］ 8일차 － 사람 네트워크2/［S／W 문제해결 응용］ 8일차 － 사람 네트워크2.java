3d954d992c42acdb8a3b0cb0b31b4859661614b9
소윤파크 <soun997@gmail.com>
2023-04-03 13:54:09 +0900

    [D6] Title: [S/W 문제해결 응용] 8일차 - 사람 네트워크2, Time: 1,494 ms, Memory: 98,100 KB -BaekjoonHub

--- SWEA/D6/1263. ［S／W 문제해결 응용］ 8일차 － 사람 네트워크2/［S／W 문제해결 응용］ 8일차 － 사람 네트워크2.java
+++ SWEA/D6/1263. ［S／W 문제해결 응용］ 8일차 － 사람 네트워크2/［S／W 문제해결 응용］ 8일차 － 사람 네트워크2.java
@@ -11,49 +11,41 @@ public class Solution {
         StringBuilder sb = new StringBuilder();
 
         int T = Integer.parseInt(br.readLine());
-        final int INF = 1001;
+        final int INF = 1000 * 1000;
 
         for (int t = 1; t <= T; t++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int N = Integer.parseInt(st.nextToken());
             int[][] dp = new int[N][N];
+
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
-                    int input = Integer.parseInt(st.nextToken());
-                    if (input == 0){
+                    dp[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if(i == j) continue;
+                    if(dp[i][j] == 0) {
                         dp[i][j] = INF;
-                        continue;
                     }
-                    dp[i][j] = input;
                 }
             }
 
             for (int k = 0; k < N; k++) {
                 for (int i = 0; i < N; i++) {
-                    if (i == k){
-                        continue;
-                    }
                     for (int j = 0; j < N; j++) {
-                        if (i == j){
-                            dp[i][j] = 0;
-                            continue;
-                        }
-                        if (j == k){
-                            continue;
-                        }
                         dp[i][j] = Math.min(dp[i][k] + dp[k][j], dp[i][j]);
                     }
                 }
             }
 
-            int min = Integer.MAX_VALUE;
+            int min = INF;
             for (int i = 0; i < N; i++) {
                 int sum = 0;
                 for (int j = 0; j < N; j++) {
                     sum += dp[i][j];
-                    if (sum > min){
-                        break;
-                    }
                 }
                 min = Math.min(min, sum);
             }
