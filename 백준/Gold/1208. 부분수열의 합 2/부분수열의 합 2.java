f264fadf5d166fb915d4cbecb3eb1e0de7b694b4
소윤파크 <soun997@gmail.com>
2023-04-03 21:05:03 +0900

    [Gold I] Title: 부분수열의 합 2, Time: 1136 ms, Memory: 100636 KB -BaekjoonHub

---
+++ 백준/Gold/1208. 부분수열의 합 2/부분수열의 합 2.java
@@ -0,0 +1,95 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, S;
+    static int[] numbers;
+    static boolean[] visited;
+    static List<Long> left, right;
+    static long total;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
+
+        numbers = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+
+        left = new ArrayList<>();
+        right = new ArrayList<>();
+        visited = new boolean[N];
+        powerSet(0, N / 2, 0, false);
+        powerSet(N / 2, N, 0, true);
+
+        // 정렬 -> 이분탐색 할라고
+        Collections.sort(left);
+        Collections.sort(right);
+
+        total = 0;
+        for (int i = 0; i < left.size(); i++) {
+            total = total + upperBound(left.get(i)) - lowerBound(left.get(i));
+        }
+        if (S == 0){
+            System.out.println(total - 1);
+        }
+        else {
+            System.out.println(total);
+        }
+    }
+
+    static void powerSet(int n, int r, long sum, boolean isRight){
+        if (n == r){
+            if (isRight){
+                right.add(sum);
+                return;
+            }
+            left.add(S - sum);
+            return;
+        }
+
+        visited[n] = true;
+        powerSet(n + 1, r, sum + numbers[n], isRight);
+        visited[n] = false;
+        powerSet(n + 1, r, sum, isRight);
+    }
+
+    static long lowerBound(long target){
+        int start = 0;
+        int end = right.size();
+        while (start < end){
+            int mid = (start + end) / 2;
+            if (right.get(mid) >= target){
+                end = mid;
+                continue;
+            }
+            start = mid + 1;
+        }
+        return start;
+    }
+
+    static long upperBound(long target){
+        int start = 0;
+        int end = right.size();
+        while (start < end){
+            int mid = (start + end) / 2;
+            if (right.get(mid) > target){
+                end = mid;
+                continue;
+            }
+            start = mid + 1;
+        }
+        return start;
+    }
+}
