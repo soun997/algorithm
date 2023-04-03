4f476faaf2075234503bc077d3a1076d26f5138d
소윤파크 <soun997@gmail.com>
2023-04-03 13:33:54 +0900

    [D6] Title: [S/W 문제해결 응용] 8일차 - 사람 네트워크2, Time: 3,360 ms, Memory: 96,664 KB -BaekjoonHub

---
+++ SWEA/D6/1263. ［S／W 문제해결 응용］ 8일차 － 사람 네트워크2/［S／W 문제해결 응용］ 8일차 － 사람 네트워크2.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+        final int INF = 1001;
+
+        for (int t = 1; t <= T; t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int[][] dp = new int[N][N];
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    int input = Integer.parseInt(st.nextToken());
+                    if (input == 0){
+                        dp[i][j] = INF;
+                        continue;
+                    }
+                    dp[i][j] = input;
+                }
+            }
+
+            for (int k = 0; k < N; k++) {
+                for (int i = 0; i < N; i++) {
+                    if (i == k){
+                        continue;
+                    }
+                    for (int j = 0; j < N; j++) {
+                        if (j == k || j == i){
+                            continue;
+                        }
+                        dp[i][j] = Math.min(dp[i][k] + dp[k][j], dp[i][j]);
+                    }
+                }
+            }
+
+            int min = Integer.MAX_VALUE;
+            for (int i = 0; i < N; i++) {
+                int sum = 0;
+                for (int j = 0; j < N; j++) {
+                    if (dp[i][j] == INF){
+                        continue;
+                    }
+                    sum += dp[i][j];
+                }
+                min = Math.min(min, sum);
+            }
+
+            sb.append("#").append(t).append(" ").append(min).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
