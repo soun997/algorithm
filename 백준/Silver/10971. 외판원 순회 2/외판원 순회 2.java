f54351c4f4d01bd6867f1628a524e8aa06754c5f
소윤파크 <soun997@gmail.com>
2023-04-06 15:51:05 +0900

    [Silver II] Title: 외판원 순회 2, Time: 84 ms, Memory: 12312 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
@@ -1,59 +1,94 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
     static int N;
-    static int[][] W;
-    static boolean[] visited;
-    static int startCity;
-    static int minCost;
+    static int[][] weights;
+    static int[][] dp;
+    static final int INF = Integer.MAX_VALUE / 1000;
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         N = Integer.parseInt(br.readLine());
-        W = new int[N][N];
+        weights = new int[N][N];
 
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
-                W[i][j] = Integer.parseInt(st.nextToken());
+                weights[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        minCost = Integer.MAX_VALUE;
-        visited = new boolean[N];
-        for (int i = 0; i < N; i++) {
-            startCity = i;
-            visited[i] = true;
-            visitCity(i, 1, 0);
-            visited[i] = false;
-        }
-        System.out.println(minCost);
+        // dp 배열을 초기화한다
+        init();
+
+        // 0번째 도시부터 순회를 시작한다
+        // 0번째 도시를 방문 처리 -> visited: 0001
+        System.out.println(visitCity(0, 1));
     }
 
-    static void visitCity(int cityNo, int count, int cost){
-        if (minCost < cost){
-            return;
+    static void init(){
+        dp = new int[1 << N][N];
+        for (int i = 0; i < (1 << N); i++) {
+            Arrays.fill(dp[i], -1);
         }
-        // 다시 처음 도시로 돌아가기
-        if (count == N){
-            // 처음 도시로 돌아갈 수 있을 때만
-            if (W[cityNo][startCity] != 0){
-                minCost = Math.min(minCost, cost + W[cityNo][startCity]);
+    }
+
+    // 비트마스킹을 이용한 도시 방문 체크
+    static int visitCity(int cityNo, int visited){
+
+        // 모든 도시 순회가 끝난 경우
+        if (visited == (1 << N) - 1) {
+
+            // 처음의 도시로 되돌아 갈 수 없다면
+            if (weights[cityNo][0] == 0) {
+                return INF;
             }
-            return;
+            // 처음의 도시로 되돌아 갈 수 있다면
+            return weights[cityNo][0];
         }
 
+        // 현재 경우의 수에서, 해당 도시를 방문한 적이 있다면 -> 중복 방문을 하지 않는다.
+        if (dp[visited][cityNo] != -1){
+            return dp[visited][cityNo];
+        }
+
+        /*
+            현재 경우의 수에서, 해당 도시를 방문한 적이 없다면
+         */
+
+        // 현재 경우의 수 기준, 해당 도시 방문 처리
+        dp[visited][cityNo] = INF;
+
         for (int i = 0; i < N; i++) {
-            // 방문할 수 없는 경우
-            if (W[cityNo][i] == 0 || visited[i]){
+
+            /*
+                다음 도시 방문
+             */
+
+            // 1. 이미 방문한 도시라면
+            // AND 연산: 해당 도시 방문 여부를 체크 -> 0001 & 0010 = 0000, 0이라면 방문하지 않은 도시
+            if ((visited & (1 << i)) != 0){
                 continue;
             }
-            visited[i] = true;
-            visitCity(i, count + 1, cost + W[cityNo][i]);
-            visited[i] = false;
+
+            // 2. 그 도시로 갈 수 있는 경로가 없다면
+            if (weights[cityNo][i] == 0){
+                continue;
+            }
+
+            // 3. 방문이 가능한 도시라면
+            // OR 연산: 해당 도시 방문 처리 -> 0001 | 0010 = 0011
+            int cost = visitCity(i, visited | (1 << i));    // 다음 도시를 방문해주고, 최소값을 반환받는다.
+            // 현재 경우의 수에서, 해당 도시로 가는 최소값 갱신
+            dp[visited][cityNo] = Math.min(cost + weights[cityNo][i], dp[visited][cityNo]);
         }
+
+        // 반환하는 값은 최소값
+        return dp[visited][cityNo];
     }
 }
