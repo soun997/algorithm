f848b05db5c74117956d063b7435b509f61ac6bd
소윤파크 <soun997@gmail.com>
2023-04-10 23:59:55 +0900

    [Gold III] Title: 레이저 통신, Time: 120 ms, Memory: 13544 KB -BaekjoonHub

--- 백준/Gold/6087. 레이저 통신/레이저 통신.java
+++ 백준/Gold/6087. 레이저 통신/레이저 통신.java
@@ -4,85 +4,77 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-
-    static int W, H;
-    static char[][] map;
-    static final int[] dx = {0, -1, 0, 1};
-    static final int[] dy = {1, 0, -1, 0};
-
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        W = Integer.parseInt(st.nextToken());
-        H = Integer.parseInt(st.nextToken());
-        map = new char[H][W];
-        int[] start = new int[2];
-        for (int i = 0; i < H; i++) {
-            char[] inputs = br.readLine().toCharArray();
-            for (int j = 0; j < W; j++) {
-                map[i][j] = inputs[j];
-                if (map[i][j] == 'C'){
-                    start[0] = i;
-                    start[1] = j;
+        int W = Integer.parseInt(st.nextToken());
+        int H = Integer.parseInt(st.nextToken());
+
+        char[][] board = new char[H][];
+
+        for (int h = 0; h < H; h++) {
+            board[h] = br.readLine().toCharArray();
+        }
+
+        Point[] points = new Point[2];
+        int cnt = 0;
+        for (int r = 0; r < H; r++) {
+            for (int c = 0; c < W; c++) {
+                if (board[r][c] == 'C') {
+                    points[cnt++] = new Point(r, c, -1, 0);
                 }
             }
         }
-
-        System.out.println(bfs(start[0], start[1]));
+        System.out.println(search(H, W, points, board));
     }
 
-    static int bfs(int x, int y){
-        map[x][y] = '.';
-        PriorityQueue<int[]> q = new PriorityQueue<>((Comparator.comparingInt(o -> o[3])));
-        // x, y, dir, 거울설치횟수
-        for (int d = 0; d < 4; d++) {
-            q.offer(new int[]{x, y, d, 0});
-        }
+    private static final int[] dr = {-1, 0, 1, 0};
+    private static final int[] dc = {0, -1, 0, 1};
 
-        int[][][] visited = new int[4][H][W];
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < H; j++){
-                Arrays.fill(visited[i][j], Integer.MAX_VALUE / 1000);
+    private static int search(int H, int W, Point[] points, char[][] board) {
+        int[][][] visit = new int[H][W][4];
+        for (int h = 0; h < H; h++) {
+            for (int w = 0; w < W; w++) {
+                Arrays.fill(visit[h][w], Integer.MAX_VALUE);
             }
         }
+        Point start = points[0];
+        Point end = points[1];
+        Queue<Point> pq = new PriorityQueue<>();
+        pq.add(start);
 
-        while (!q.isEmpty()) {
-            int[] cur = q.poll();
-            if (map[cur[0]][cur[1]] == 'C'){
-                return cur[3];
-            }
+        while (!pq.isEmpty()) {
+            Point cur = pq.poll();
+//            System.out.println(cur.r + ", " + cur.c + " : " + cur.turn);
+            if (cur.r == end.r && cur.c == end.c) return cur.turn;
             for (int d = 0; d < 4; d++) {
-                int nx = cur[0] + dx[d];
-                int ny = cur[1] + dy[d];
-                int nMirrors = (cur[2] == d) ? cur[3] : cur[3] + 1;
-
-                // 범위 벗어나거나 벽인 경우 -> 진행 불가
-                if (!check(nx, ny) || map[nx][ny] == '*'){
-                    continue;
-                }
-
-                // 정반대 -> 갈 수 없음
-                if ((cur[2] + 2) % 4 == d){
-                    continue;
-                }
-
-                if (visited[d][nx][ny] <= nMirrors){
-                    continue;
-                }
-                q.offer(new int[]{nx, ny, d, nMirrors});
-                visited[d][nx][ny] = nMirrors;
+                int nr = cur.r + dr[d];
+                int nc = cur.c + dc[d];
+                int nextTurn = cur.turn;
+                if (cur.d != -1 && cur.d != d) nextTurn++;
+                if (nr < 0 || nr >= H || nc < 0 || nc >= W || board[nr][nc] == '*' || visit[nr][nc][d] <= nextTurn) continue;
+                pq.add(new Point(nr, nc, d, nextTurn));
+                visit[nr][nc][d] = nextTurn;
             }
         }
-
-        return 0;
+        return -1;
     }
 
-    static boolean check(int x, int y){
-        if (x < 0 || x >= H || y < 0 || y >= W) {
-            return false;
+    private static class Point implements  Comparable<Point>{
+        int r, c, d;
+        int turn;
+
+        public Point(int r, int c, int d, int turn) {
+            this.r = r;
+            this.c = c;
+            this.d = d;
+            this.turn = turn;
         }
-        return true;
+
+        @Override
+        public int compareTo(Point o) {
+            return turn - o.turn;
+        }
     }
 }
