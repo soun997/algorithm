9ef0b6cd439e718d76413ee7ed597f8275714859
소윤파크 <soun997@gmail.com>
2023-04-20 09:15:32 +0900

    [Silver III] Title: N과 M (1), Time: 164 ms, Memory: 20920 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,45 +1,45 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
     static int N, M;
-    static int[] p;
-    static int[] nums;
+    static int[] choices;
     static boolean[] visited;
     static StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        M = sc.nextInt();
-        p = new int[N];
-        nums = new int[M];
+        choices = new int[M];
         visited = new boolean[N];
-        for (int i = 0; i < N; i++){
-            p[i] = i + 1;
-        }
-        perm(0);
+
+        permutation(0);
+
         System.out.println(sb);
     }
 
-    static void perm(int cnt){
-
-        if (cnt == M){
-            cnt++;
-            for (int i = 0; i < nums.length; i++) {
-                sb.append(nums[i]).append(" ");
+    static void permutation(int cnt){
+        if (cnt == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(choices[i]).append(" ");
             }
             sb.append("\n");
             return;
         }
 
         for (int i = 0; i < N; i++) {
-            if (visited[i]) continue;
+            if (visited[i]) {
+                continue;
+            }
+            choices[cnt] = i + 1;
             visited[i] = true;
-            nums[cnt] = p[i];
-            perm(cnt + 1);
-            //nums[cnt] = 0;
+            permutation(cnt + 1);
             visited[i] = false;
         }
     }
