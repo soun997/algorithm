9ec679852f88e1a70d1e7a0ef4b8f04a6668486a
소윤파크 <soun997@gmail.com>
2023-04-26 17:12:36 +0900

    [Silver III] Title: N과 M (6), Time: 76 ms, Memory: 11592 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.java
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
@@ -6,42 +7,41 @@ import java.util.StringTokenizer;
 public class Main {
 
     static int N, M;
-    static int[] p;
-    static int[] choices;
-    static StringBuilder sb = new StringBuilder();
+    static int[] numbers;
+    static int[] sequence;
+    static StringBuilder result;
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        p = new int[N];
-        choices = new int[M];
-
+        numbers = new int[N];
+        sequence = new int[M];
+        result = new StringBuilder();
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            p[i] = Integer.parseInt(st.nextToken());
+            numbers[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(p);
+        Arrays.sort(numbers);
 
         combination(0, 0);
 
-        System.out.println(sb);
+        System.out.println(result);
     }
 
-    static void combination(int cnt, int start){
-
-        if (cnt == M){
+    static void combination(int start, int cnt){
+        if (cnt == M) {
             for (int i = 0; i < M; i++) {
-                sb.append(choices[i]).append(" ");
+                result.append(sequence[i]).append(" ");
             }
-            sb.append("\n");
+            result.append("\n");
             return;
         }
 
         for (int i = start; i < N; i++) {
-            choices[cnt] = p[i];
-            combination(cnt + 1, i + 1);
+            sequence[cnt] = numbers[i];
+            combination(i + 1, cnt + 1);
         }
     }
 }
