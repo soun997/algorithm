78245df497d78dd34fc9f94aff81548914705be9
소윤파크 <soun997@gmail.com>
2023-04-26 17:16:08 +0900

    [Silver III] Title: N과 M (7), Time: 612 ms, Memory: 220024 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/*
+    N과 M (7)
+ */
+public class Main {
+
+    static int N, M;
+    static int[] numbers;
+    static int[] sequence;
+    static StringBuilder result;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        numbers = new int[N];
+        sequence = new int[M];
+        result = new StringBuilder();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(numbers);
+
+        combination(0);
+
+        System.out.println(result);
+    }
+
+    static void combination(int cnt){
+        if (cnt == M) {
+            for (int i = 0; i < M; i++) {
+                result.append(sequence[i]).append(" ");
+            }
+            result.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            sequence[cnt] = numbers[i];
+            combination(cnt + 1);
+        }
+    }
+}
