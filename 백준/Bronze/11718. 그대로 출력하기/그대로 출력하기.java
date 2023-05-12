8dc1740db5837a60629d323f001cec19d12a9b7a
소윤파크 <soun997@gmail.com>
2023-05-12 23:17:28 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 80 ms, Memory: 11644 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String input = "";
+        while((input = br.readLine()) != null){
+            sb.append(input).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
