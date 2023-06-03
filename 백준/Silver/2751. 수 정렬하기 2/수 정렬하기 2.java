877215beaf748bf7eed5e772f453f25b7fd63a87
소윤파크 <soun997@gmail.com>
2023-06-04 01:06:54 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 724 ms, Memory: 138324 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+// 수 정렬하기 2
+public class Main {
+
+    static final int OFFSET = 2_000_000;
+    static int N;
+    static int[] numbers;
+    static int max;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        numbers = new int[OFFSET * 2 + 1];
+        max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(br.readLine());
+            numbers[num + OFFSET]++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i <= OFFSET * 2; i++) {
+            if (numbers[i] != 0){
+                sb.append(i - OFFSET).append("\n");
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
