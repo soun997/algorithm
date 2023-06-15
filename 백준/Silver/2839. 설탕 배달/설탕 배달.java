31ecf87347bdcef32e402a260b482decafffbbc8
소윤파크 <soun997@gmail.com>
2023-06-16 00:59:06 +0900

    [Silver IV] Title: 설탕 배달, Time: 80 ms, Memory: 11512 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -3,22 +3,41 @@ import java.io.InputStreamReader;
 
 public class Main {
 
+    static int N;
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
-        
-        if (N == 4 || N == 7) {
+        N = Integer.parseInt(br.readLine());
+
+        // 먼저, 5kg으로 나눴을 때 남은 설탕을 3kg으로 나눌 수 없다면
+        if ((N % 5) % 3 != 0){
+            int a = 0;
+            int b = 0;
+
+            // 3키로 봉지로 만들 수 있는 수: 3, 6, 9, 12 -> 15는 3과 5의 최대공배수이므로 5로도 나누어짐!!
+            // 고로 3키로씩 최대 4번 뺐을 때까지 답이 나오지 않는 다면 N킬로그램을 만들 수 없음
+            for (int i = 0; i < 4; i++) {
+                b++;    // 3키로짜리 봉지 추가
+                N = N - 3;  // 3kg 하나 빼줌
+
+                // N이 음수까지 내려갔다면 -> N킬로그램 만들기 불가
+                if (N < 0){
+                    break;
+                }
+                if ((N % 5) % 3 == 0){
+                    a = N / 5;
+                    b = b + (N % 5) / 3;
+                    System.out.println(a + b);
+                    return;
+                }
+            }
             System.out.println(-1);
+            return;
         }
-        else if (N % 5 == 0) {
-            System.out.println(N / 5);
-        }
-        else if (N % 5 == 1 || N % 5 == 3) {
-            System.out.println((N / 5) + 1);
-        }
-        else if (N % 5 == 2 || N % 5 == 4) {
-            System.out.println((N / 5) + 2);
-        }
+
+        int a = N / 5;
+        int b = (N % 5) / 3;
+        System.out.println(a + b);
     }
 }
