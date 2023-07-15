d2ddc6b694b56398b73da71c099a40db504dff1a
소윤파크 <soun997@gmail.com>
2023-07-15 22:41:59 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 45.94 ms, Memory: 116 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -0,0 +1,70 @@
+import java.util.*;
+
+class Solution {
+    
+    Queue<Long> q1 = new ArrayDeque<>();
+    Queue<Long> q2 = new ArrayDeque<>();
+    long sumQ1;
+    long sumQ2;
+    
+    
+    public int solution(int[] queue1, int[] queue2) {
+        
+        init(queue1, queue2);
+        
+        return makeEqual();
+    }
+    
+    void init(int[] queue1, int[] queue2) {
+        
+        for (int i = 0; i < queue1.length; i++) {
+            q1.offer((long)queue1[i]);
+            sumQ1 += queue1[i];
+        }
+        
+        for (int i = 0; i < queue2.length ; i++) {
+            q2.offer((long)queue2[i]);
+            sumQ2 += queue2[i];
+        }
+    }
+    
+    int makeEqual(){
+        
+        int count = 0;
+        
+        while(!q1.isEmpty() && !q2.isEmpty()) {
+            if (count > 300000) {
+                break;
+            }
+            
+            if (sumQ1 == sumQ2) {
+                return count; 
+            }
+
+            if (sumQ1 < sumQ2) {
+                long num = q2.poll();
+                sumQ2 -= num;
+                q1.offer(num);
+                sumQ1 += num;
+                count++;
+                continue;
+            }
+            if (sumQ1 > sumQ2) {
+                long num = q1.poll();
+                sumQ1 -= num;
+                q2.offer(num);
+                sumQ2 += num;
+                count++;
+            }
+        }
+        
+        return -1;
+    }
+    
+    void printQueue(Queue<Long> q) {
+        for (Long num : q) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
