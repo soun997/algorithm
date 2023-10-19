6ad3224550bec8958ddb6d3c4af0be355eb99172
소윤파크 <soun997@gmail.com>
2023-10-19 09:20:41 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 5.49 ms, Memory: 88.1 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/README.md
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 쿼드압축 후 개수 세기 - 68936 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68936) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68936#) 
 
 ### 성능 요약
 
-메모리: 399 MB, 시간: 1140.41 ms
+메모리: 88.1 MB, 시간: 5.49 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 3일 20:10:51
+2023년 10월 4일 9:20:35
 
 ### 문제 설명
 
--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -1,79 +1,49 @@
+
 import java.util.*;
 
 class Solution {
-    
-    int[][] arr;
-    int length;
-    int[] answer;
-    
-    int[] dx = {-1, 0, 1, 0};
-    int[] dy = {0, -1, 0, 1};
-    
-    public int[] solution(int[][] arr) {
-        this.arr = arr;
-        this.length = arr.length;
-        this.answer = new int[2];
-        if (isPressed(0, 0, arr[0][0], length)) {
-            answer[arr[0][0]]++;
-            return answer;
-        }
-        divide(0, 0, length / 2);
-        
+    static int[] answer;
+    private static int m;
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution(new int[][]{{1, 1, 0, 0}, {1, 0, 0, 0}, {1, 0, 0, 1}, {1, 1, 1, 1}})));
+    }
+
+    public static int[] solution(int[][] arr) {
+        answer = new int[2];
+        int n = arr.length;
+
+        divide(0, 0, n, arr);
+
         return answer;
     }
-    
-    int[] wx = {0, 0, 1, 1};
-    int[] wy = {0, 1, 0, 1};
-    
-    public void divide(int x, int y, int n) {
-        
-        for (int d = 0; d < 4; d++) {
-            int nx = x + wx[d] * n;
-            int ny = y + wy[d] * n;
-            if (isPressed(nx, ny, arr[nx][ny], n)) {
-                answer[arr[nx][ny]]++;
-                continue;
-            }
-            divide(x + wx[d] * n, y + wy[d] * n, n / 2);
-        }
-    }
-    
-    public boolean isPressed(int x, int y, int origin, int n) {
-        if (n == 1) {
-            return true;
-        }
-        Queue<int[]> q = new ArrayDeque<>();
-        boolean[][] visited = new boolean[length][length];
-        q.offer(new int[]{x, y});
-        visited[x][y] = true;
-        
-        while(!q.isEmpty()) {
-            int[] cur = q.poll();
-            if (arr[cur[0]][cur[1]] != origin) {
-                return false;
-            }
-            for (int d = 0; d < 4; d++) {
-                int nx = cur[0] + dx[d];
-                int ny = cur[1] + dy[d];
-                if (isOutOfBounds(nx, ny, x, y, n) || 
-                    visited[nx][ny]) {
-                    continue;
+
+    private static boolean check(int row, int col, int n, int[][] map) {
+        int std = map[row][col];
+        for (int i = row; i < row + n; i++) {
+            for (int j = col; j < col + n; j++) {
+                if (std != map[i][j]) {
+                    return false;
                 }
-                q.offer(new int[]{nx, ny});
-                visited[nx][ny] = true;
             }
         }
-        
+        m = std;
         return true;
     }
-    
-    private boolean isOutOfBounds(int x, int y, 
-                                  int startX, int startY, 
-                                  int range){
-        if (x < startX || x >= startX + range || 
-            y < startY || y >= startY + range) {
-            return true;
+
+    private static void divide(int row, int col, int n, int[][] map) {
+        if (check(row, col, n, map)) {
+            if (m == 0)
+                answer[0]++;
+            else
+                answer[1]++;
+        } else {
+            int s = n / 2;
+            for (int i = 0; i < 2; i++) {
+                for (int j = 0; j < 2; j++) {
+                    divide(row + s * i, col + s * j, s, map);
+                }
+            }
         }
-        return false;
     }
 }
