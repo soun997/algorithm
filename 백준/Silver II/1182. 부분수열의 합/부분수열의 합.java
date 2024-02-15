a8f9f457c581ca3e22699ec0daf12330b05a5ad7
소윤파크 <soun997@gmail.com>
2024-02-15 19:59:39 +0900

    [Silver II] Title: 부분수열의 합, Time: 96 ms, Memory: 11684 KB -BaekjoonHub

--- 백준/Silver II/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver II/1182. 부분수열의 합/부분수열의 합.java
@@ -24,32 +24,30 @@ public class Main {
             numbers[i] = Integer.parseInt(st.nextToken());
         }
 
-        powerSet(0);
+        powerSet(0, 0);
+        // 공집합을 빼주어야 함
+        if (S == 0) {
+            total--;
+        }
         System.out.println(total);
     }
 
-    static void powerSet(int idx) {
+    static void powerSet(int idx, int sum) {
         if (idx == N) {
-            if (isPossible()) {
+            if (sum == S) {
                 total++;
             }
             return;
         }
+        /**
+         * 부분집합을 구하는 방법
+         * 원소의 개수가 N개일 때,
+         * 각각의 원소는 포함될 수도 있고 포함되지 않을 수도 있다.
+         * 그러므로 경우의 수는 2^N (공집합 포함)
+         */
         visited[idx] = false;
-        powerSet(idx + 1);
+        powerSet(idx + 1, sum);
         visited[idx] = true;
-        powerSet(idx + 1);
-    }
-
-    static boolean isPossible() {
-        int sum = 0;
-        boolean isEmptySet = true;
-        for (int i = 0; i < N; i++) {
-            if (visited[i]) {
-                sum += numbers[i];
-                isEmptySet = false;
-            }
-        }
-        return (sum == S) && !isEmptySet;
+        powerSet(idx + 1, sum + numbers[idx]);
     }
 }
