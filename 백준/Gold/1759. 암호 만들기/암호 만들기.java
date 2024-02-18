165b93de1e794309c535f90388ec25892ca24b2f
소윤파크 <soun997@gmail.com>
2024-02-18 21:41:02 +0900

    [Gold V] Title: 암호 만들기, Time: 84 ms, Memory: 11540 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -1,15 +1,14 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
 
     static int L, C;
     static int[] alphabets;
-    static boolean[] visited;
-    static Set<Character> vowels = Set.of('a', 'i', 'o', 'u', 'e');
+    static boolean[] visited = new boolean[26];
+    static int[] vowels = { 'a', 'i', 'o', 'u', 'e' };
     static StringBuilder result = new StringBuilder();
 
     // 한 개 이상의 모음, 두 개 이상의 자음, 알파벳은 정렬
@@ -19,10 +18,9 @@ public class Main {
         L = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
         alphabets = new int[C];
-        visited = new boolean[C];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < C; i++) {
-            alphabets[i] = st.nextToken().charAt(0);
+            alphabets[i] = st.nextToken().toCharArray()[0];
         }
         Arrays.sort(alphabets);
         combination(0, 0);
@@ -36,7 +34,7 @@ public class Main {
             if (isPossible()) {
                 for (int i = 0; i < visited.length; i++) {
                     if (visited[i]) {
-                        result.append(((char)alphabets[i]));
+                        result.append(((char)(i + 97)));
                     }
                 }
                 result.append("\n");
@@ -46,18 +44,18 @@ public class Main {
 
         for (int i = idx; i < C; i++) {
 
-            visited[i] = true;
+            visited[alphabets[i] - 97] = true;
             combination(cnt + 1, i + 1);
-            visited[i] = false;
+            visited[alphabets[i] - 97] = false;
         }
     }
 
     static boolean isPossible() {
 
         int cnt = 0;
-        for (int i = 0; i < visited.length; i++) {
+        for (int i = 0; i < vowels.length; i++) {
 
-            if (visited[i] && vowels.contains((char)alphabets[i])) {
+            if (visited[vowels[i] - 97]) {
                 cnt++;
             }
         }
