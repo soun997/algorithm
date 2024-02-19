4a744f2f8af3b8d2b4374407348fedbd5fae70ba
소윤파크 <soun997@gmail.com>
2024-02-19 15:24:37 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 84 ms, Memory: 11460 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
@@ -3,48 +3,37 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int n;
-	static int[] sour;
-	static int[] bitter;
-	static boolean[] visited;
-	static int min = Integer.MAX_VALUE;
 
-	public static void main(String[] args) throws Exception {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(br.readLine());
-		visited = new boolean[n];
-		sour = new int[n];
-		bitter = new int[n];
-		for (int i = 0; i < n; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			sour[i] = Integer.parseInt(st.nextToken());
-			bitter[i] = Integer.parseInt(st.nextToken());
-		}
-		powerSet(0, 0, 1);
-		System.out.println(min);
-	}
-	
-	static void powerSet(int cnt, int tot, int mul) {
-		
-		if (cnt == n) {
-/*			for (int i = 0; i < n; i++) {
-				if (visited[i]) {
-					System.out.printf("%d ", bitter[i]);
-					System.out.printf("%d ", sour[i]);
-				}
-			}*/
-			//System.out.println();
-			if (tot == 0) return;
-			min = Math.min(min,  Math.abs(tot - mul));
-			//System.out.println(tot + " " + mul);
-			return;
-		}
-		
-		visited[cnt] = true;
-		powerSet(cnt + 1, tot + bitter[cnt], mul * sour[cnt]);
-		visited[cnt] = false;
-		powerSet(cnt + 1, tot, mul);
-	}
+    static int N;
+    static int[][] flavors;
+    static int min = Integer.MAX_VALUE;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        flavors = new int[N][2];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            flavors[i][0] = Integer.parseInt(st.nextToken());
+            flavors[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        powerSet(0, 1, 0);
+        System.out.println(min);
+    }
+
+    // 물은 요리가 아니다 -> 공집합 제외
+    static void powerSet(int cnt, int sour, int bitter) {
+
+        if (cnt == N) {
+
+            if (sour != 1 || bitter != 0) {
+                min = Math.min(min, Math.abs(sour - bitter));
+            }
+            return;
+        }
+
+        powerSet(cnt + 1, sour * flavors[cnt][0], bitter + flavors[cnt][1]);
+        powerSet(cnt + 1, sour, bitter);
+    }
 }
