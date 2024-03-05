52b5c220306327bc12be5e121865ad7d013d4117
소윤파크 <soun997@gmail.com>
2024-03-05 15:34:42 +0900

    [Gold I] Title: 마법사 상어와 블리자드, Time: 160 ms, Memory: 13400 KB -BaekjoonHub

--- 백준/Gold/21611. 마법사 상어와 블리자드/마법사 상어와 블리자드.java
+++ 백준/Gold/21611. 마법사 상어와 블리자드/마법사 상어와 블리자드.java
@@ -1,149 +1,192 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
     static int N, M;
-    static int[][] map;
-    static List<Integer> marbles;
-    static int[] result;
+    static int[][] matrix;
+    static int[] line;
+    static int[] move;
+    static int marbleLength;    // 구슬이 마지막으로 채워져 있는 index
+    static int[] explodedMarbles = new int[4];
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int[] init = {7, 3, 1, 5};
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        map = new int[N][N];
-        marbles = new ArrayList<>();
-        marbles.add(-1);
-        result = new int[4];
-
-        // 구슬 정보
+        matrix = new int[N][N];
+        line = new int[N * N];
+        move = new int[N * N];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
+                matrix[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-
-        init(N / 2, N / 2); // 초기화
-
+        fillLine();
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int dir = Integer.parseInt(st.nextToken()) - 1;
-            int distance = Integer.parseInt(st.nextToken());
-            blizzard(dir, distance);
+            int d = Integer.parseInt(st.nextToken());
+            int s = Integer.parseInt(st.nextToken());
+            blizzard(d - 1, s);
+//            System.out.println(Arrays.toString(line));
+//            System.out.println(marbleLength);
         }
 
-        System.out.println(result[1] + 2 * result[2] + 3 * result[3]);
+//        System.out.println(Arrays.toString(explodedMarbles));
+        System.out.println(explodedMarbles[1] + 2 * explodedMarbles[2] + 3 * explodedMarbles[3]);
     }
 
-    // 달팽이 모양으로 맵을 채운다
-    // 구슬 리스트를 만든다
-    static void init(int x, int y) {
-        // 좌, 하, 우, 상
-        int[] dx = {0, 1, 0, -1};
-        int[] dy = {-1, 0, 1, 0};
+    static void blizzard(int d, int s) {
 
-        int idx = 1;
-        int max = 1;
-        int dir = 0;
-        while (true) {
-            for (int i = 0; i < 2; i++) {
-                for (int k = 0; k < max; k++) {
-                    x = x + dx[dir];
-                    y = y + dy[dir];
-                    if (map[x][y] != 0) {
-                        marbles.add(map[x][y]);
-                    }
-                    map[x][y] = idx++;
-                    if (x == 0 && y == 0) {
-                        return;
-                    }
-                }
-                dir = (dir + 1) % 4;
-            }
-            max++;
-        }
-    }
-
-    // 블리자드를 쏜다
-    static void blizzard(int dir, int distance) {
-        // 상, 하, 좌, 우
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
-
-        int x = N / 2;
-        int y = N / 2;
-        int idx = 0;
-        while (distance > 0) {
-            x = x + dx[dir];
-            y = y + dy[dir];
-            if (map[x][y] - idx > marbles.size() - 1){
+        int idx = init[d];
+        int delta = idx + 8;
+        int removed = 0;
+        for (int i = 0; i < s; i++) {
+            if (line[idx] == 0) {
                 break;
             }
-            marbles.remove(map[x][y] - idx++);  // primitive 타입이기 때문에 index 삭제로 수행
-            distance--;
+            line[idx] = 0;
+            updateStatus(idx + 1, 1);
+            idx = idx + delta;
+            delta = delta + 8;
+            removed++;
         }
-
-        while (explosion()){}
-        transform();
+        removeZeros();
+        marbleLength -= removed;
+        while(explode());
+        change();
     }
 
-    // 연속된 구슬을 폭발시킨다
-    static boolean explosion() {
+    static boolean explode() {
 
-        boolean flag = false;
-        int conscious = 1;
-        for (int i = marbles.size() - 1; i > 0; i--) {
+        int exploded = 0;
+        for (int i = 1; i <= marbleLength;) {
 
-            // 연속된 구슬
-            if (marbles.get(i) == marbles.get(i - 1)) {
-                conscious++;
-            } else {
-                if (conscious >= 4) {
-                    flag = true;
-                    int num = marbles.get(i);
-                    result[num] = result[num] + conscious;
-                    for (int k = i + conscious - 1; k >= i; k--) {
-                        marbles.remove(k);
-                    }
+            int cnt = count(i, line[i]);
+            if (cnt >= 4) {
+                explodedMarbles[line[i]] += cnt;
+                for (int j = i; j < i + cnt; j++) {
+                    line[j] = 0;
                 }
-                conscious = 1;
+                updateStatus(i + cnt, cnt);
+                exploded += cnt;
             }
+            i += cnt;
         }
-
-        return flag;
+        removeZeros();
+        marbleLength -= exploded;
+        return exploded != 0;
     }
 
-    static void transform() {
-        List<Integer> newMarbles = new ArrayList<>();
-        int marbleCount = marbles.size();
-        int left = 1;
-        int right = 1;
-        while (right < marbleCount) {
-            if (marbles.get(left).equals(marbles.get(right))) {
-                right++;
+    static void change() {
+
+        int idx = 1;
+        int[] newLine = new int[N * N];
+        for (int i = 1; i <= marbleLength;) {
+            int count = count(i, line[i]);
+            if (idx >= N * N) {
+                break;
+            }
+            newLine[idx++] = count;
+            if (idx >= N * N) {
+                break;
+            }
+            newLine[idx++] = line[i];
+            i += count;
+        }
+
+        line = newLine;
+        marbleLength = idx - 1;
+    }
+
+    static int count(int start, int num) {
+
+        int cnt = 0;
+        for (int i = start; i <= marbleLength; i++) {
+            if (num == line[i]) {
+                cnt++;
                 continue;
             }
+            break;
+        }
+        return cnt;
+    }
 
-            int num = marbles.get(left);
-            newMarbles.add(right - left);
-            newMarbles.add(num);
-            left = right;
+    static void removeZeros() {
+        for (int i = 1; i <= marbleLength; i++) {
+            line[i - move[i]] = line[i];
+            move[i] = 0;
+        }
+    }
+
+    static void updateStatus(int start, int delta) {
+
+        for (int i = start; i <= marbleLength; i++) {
+            move[i] += delta;
+        }
+    }
+
+    static void fillLine() {
+        int[] dx = { 0, -1, 0, 1};
+        int[] dy = { 1, 0, -1, 0};
+
+        int length = 2;
+
+        line[1] = matrix[(N + 1) / 2 - 1][(N + 1) / 2 - 2];
+        line[2] = matrix[(N + 1) / 2][(N + 1) / 2 - 2];
+        if (line[1] == 0) {
+            return;
+        }
+        if (line[2] == 0) {
+            length = 1;
+        } else {
+            length = 2;
         }
 
-        if (left < marbleCount) {
-            int num = marbles.get(left);
-            newMarbles.add(right - left);
-            newMarbles.add(num);
-        }
+        int idx = 3;
+        int r = (N + 1) / 2;
+        int c = (N + 1) / 2 - 2;
 
-        marbles = new ArrayList<>(newMarbles.subList(0, Math.min(newMarbles.size(), N * N - 1)));
-        marbles.add(0, -1);
+        while(true) {
+
+            for (int d = 0; d < 2; d++) {
+                for (int i = 0; i < length; i++) {
+                    r = r + dx[d];
+                    c = c + dy[d];
+                    if (isOutOfBounds(r, c)) {
+                        marbleLength = idx - 1;
+                        return;
+                    }
+                    line[idx++] = matrix[r][c];
+                }
+            }
+            length++;
+            for (int d = 2; d < 4; d++) {
+                for (int i = 0; i < length; i++) {
+                    r = r + dx[d];
+                    c = c + dy[d];
+                    if (isOutOfBounds(r, c)) {
+                        marbleLength = idx - 1;
+                        return;
+                    }
+                    line[idx++] = matrix[r][c];
+                }
+            }
+            length++;
+        }
+    }
+
+    static boolean isOutOfBounds(int x, int y) {
+        if (x < 0 || x >= N || y < 0 || y >= N || matrix[x][y] == 0) {
+            return true;
+        }
+        return false;
     }
 }
