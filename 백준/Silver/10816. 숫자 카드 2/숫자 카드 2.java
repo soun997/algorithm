b9ff263c5d14f809c5c26a70e4ae97b1b3b03e35
소윤파크 <soun997@gmail.com>
2024-03-20 18:43:42 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1232 ms, Memory: 182244 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -5,61 +5,63 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int N;
-    static int M;
-    static int[] nArr;
+    static int N, M;
+    static int[] have;
+    static int[] count;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
         N = Integer.parseInt(br.readLine());
-        nArr = new int[N];
+        have = new int[N + 1];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            nArr[i] = Integer.parseInt(st.nextToken());
+            have[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(nArr);
-
+        have[N] = Integer.MAX_VALUE;
+        Arrays.sort(have);
         M = Integer.parseInt(br.readLine());
+        count = new int[M];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < M; i++) {
             int target = Integer.parseInt(st.nextToken());
-            int lowerIdx = lowerIndex(target, N);
-            int upperIdx = upperIndex(target, N);
-            sb.append(upperIdx - lowerIdx).append(" ");
+            int end = upperBound(target);
+            int start = lowerBound(target);
+            count[i] = end - start;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int c : count) {
+            sb.append(c).append(" ");
         }
         System.out.println(sb);
     }
+    static int upperBound(int target) {
 
-    // start = end가 될 경우 -> 가능한 후보를 하나 찾은 것 (가장 처음 나타나는 index)
-    static int lowerIndex(int target, int length){
         int start = 0;
-        int end = length;
-        while (start < end){
+        int end = N;
+        while(start < end) {
             int mid = (start + end) / 2;
-            if (nArr[mid] < target){
+            if (have[mid] <= target) {
                 start = mid + 1;
+                continue;
             }
-            if (nArr[mid] >= target){
-                end = mid;
-            }
+            end = mid;
         }
-        return start;
+        return end;
     }
 
-    static int upperIndex(int target, int length){
+    static int lowerBound(int target) {
+
         int start = 0;
-        int end = length;
-        while (start < end){
+        int end = N;
+        while(start < end) {
             int mid = (start + end) / 2;
-            if (nArr[mid] <= target){
+            if (have[mid] < target) {
                 start = mid + 1;
+                continue;
             }
-            if (nArr[mid] > target){
-                end = mid;
-            }
+            end = mid;
         }
-        return start;
+        return end;
     }
 }
