656dee6cf2aa8851572623253a7c9a8273417f0a
소윤파크 <soun997@gmail.com>
2024-03-22 11:17:01 +0900

    [Gold IV] Title: 연구소, Time: 440 ms, Memory: 143356 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -54,14 +54,15 @@ public class Main {
             // 벽 세울 위치를 정했으니 세움
             for (int idx : choices) {
                 int[] pos = blanks.get(idx);
-                copied[pos[0]][pos[1]] = 1;
+                copied[pos[0]][pos[1]] = WALL;
             }
             // 바이러스 퍼짐
+            int safeZones = blanks.size() - WALL_MAX;
             for (int[] pos : viruses) {
-                virus(pos[0], pos[1], copied);
+                safeZones -= virus(pos[0], pos[1], copied);
             }
             // 안전영역 구하기
-            max = Math.max(max, countSafeZone(copied));
+            max = Math.max(max, safeZones);
             return;
         }
         for (int i = start; i < blanks.size(); i++) {
@@ -71,38 +72,29 @@ public class Main {
         }
     }
 
-    static void virus(int x, int y, int[][] copied) {
+    static int virus(int x, int y, int[][] copied) {
         Queue<int[]> q = new ArrayDeque<>();
         boolean[][] visited = new boolean[N][M];
         q.offer(new int[]{x, y});
         visited[x][y] = true;
+        int spreaded = 0;
         while (!q.isEmpty()) {
             int[] cur = q.poll();
             for (int d = 0; d < 4; d++) {
                 int nx = cur[0] + dx[d];
                 int ny = cur[1] + dy[d];
-                // 이동할 수 없는 경우 -> 범위 초과, 빈칸이 아닌 경우
-                if (isOutOfBounds(nx, ny) || copied[nx][ny] != BLANK) {
+                // 이동할 수 없는 경우 -> 범위 초과, 이미 방문, 빈칸이 아닌 경우
+                if (isOutOfBounds(nx, ny) || visited[nx][ny] || copied[nx][ny] != BLANK) {
                     continue;
                 }
                 // 이동할 수 있는 경우
                 copied[nx][ny] = VIRUS;
                 q.offer(new int[]{nx, ny});
                 visited[nx][ny] = true;
+                spreaded++;
             }
         }
-    }
-
-    static int countSafeZone(int[][] copied) {
-        int cnt = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (copied[i][j] == 0) {
-                    cnt++;
-                }
-            }
-        }
-        return cnt;
+        return spreaded;
     }
 
     static boolean isOutOfBounds(int x, int y) {
@@ -118,12 +110,5 @@ public class Main {
             }
         }
         return copied;
-    }
-
-    static void print() {
-        for (int i = 0; i < N; i++) {
-            System.out.println(Arrays.toString(lab[i]));
-        }
-        System.out.println();
     }
 }
