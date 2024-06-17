0fb24f7b0fe365e5711b7de71e391e09c686f300
소윤파크 <soun997@gmail.com>
2024-06-17 09:49:04 +0900

    [level 2] Title: 숫자의 표현, Time: 1.18 ms, Memory: 69.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int solution(int n) {
+        int lower = 0;
+        int upper = 1;
+        int answer = 0;
+        
+        while(upper <= n && lower <= n) {
+            int sum = getSum(lower, upper);
+            if (sum < n) {
+                upper++;
+            }
+            if (sum == n) {
+                answer++;
+                upper++;
+            }
+            if (sum > n) {
+                lower++;
+            }
+        }
+        
+        return answer;
+    }
+    
+    private int getSum(int lower, int upper) {
+        int a = lower * (lower + 1) / 2;
+        int b = upper * (upper + 1) / 2;
+        return b - a;
+    }
+}
