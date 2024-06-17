4d22f6b3a12b01e5a01b29249b812df13a32ca90
소윤파크 <soun997@gmail.com>
2024-06-17 09:50:15 +0900

    [level 2] Title: 숫자의 표현, Time: 1.13 ms, Memory: 51.9 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -11,6 +11,7 @@ class Solution {
             }
             if (sum == n) {
                 answer++;
+                lower++;
                 upper++;
             }
             if (sum > n) {
