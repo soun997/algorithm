7d2060ce4f7954b4a8f3daf032c0b20a9c5fc61a
소윤파크 <soun997@gmail.com>
2024-09-02 03:03:34 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 708.92 ms, Memory: 531 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
@@ -1,46 +1,70 @@
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 class Solution {
+    
+    Map<Integer, List<Integer>> outputGraph = new HashMap<>();
+    Map<Integer, List<Integer>> inputGraph = new HashMap<>();
+    boolean[] visited;
+    int[] answer = new int[4];
+    
     public int[] solution(int[][] edges) {
-        Map<Integer, int[]> nodeCnt = new HashMap<>();
-    	int[] answer = {0, 0, 0, 0};
-    	
-    	Arrays.stream(edges).forEach(edge -> {
-    		int a = edge[0];
-    		int b = edge[1];
-    		if(!nodeCnt.containsKey(a)) {
-    			nodeCnt.put(a, new int[] {0, 0});
-    		}
-    		if(!nodeCnt.containsKey(b)) {
-    			nodeCnt.put(b, new int[] {0, 0});
-    		}
-    		// 가는것, 들어오는것 카운터임
-    		nodeCnt.get(a)[0] += 1;
-    		nodeCnt.get(b)[1] += 1;
-    	});
-    	
-    	int[] cnts;
-    	for(int key : nodeCnt.keySet()) {
-    		cnts = nodeCnt.get(key);
-    		
-    		// 들어오션 노드가 없고 나가는 노드가 2개 이상일때 정점이 된다.
-    		if(cnts[0] >= 2 && cnts[1] == 0 ) {
-    			answer[0] = key;
-    		// 들어오는 정점의 개수가 막대 그래프임 개수
-    		}else if(cnts[0] == 0 && cnts[1] > 0) {
-    			answer[2]++;
-    		// 들어오는것 나가는것 각 2개 이상인 점의 개수는 8자 그래프의 개수
-    		}else if(cnts[0] >= 2 && cnts[1] >= 2) {
-    			answer[3]++;
-    		}
-    	
-    	}
-    	
-    	// 점정 나가는 노드 수가 막대와 8자를 제외한것이 도넛 그래프의 개수
-        answer[1] = nodeCnt.get(answer[0])[0] - answer[2] - answer[3];
+        initGraph(edges);
+        visited = new boolean[outputGraph.size() + 1];
         
+        int generatedVertex = findGeneratedVertex();
+        answer[0] = generatedVertex;
+        
+        for (int start : outputGraph.get(generatedVertex)) {
+            answer[getGraphType(start)]++;
+        }
         return answer;
+    }
+    
+    private void initGraph(int[][] edges) {
+        for (int[] edge : edges) {
+            int from = edge[0];
+            int to = edge[1];
+            outputGraph.putIfAbsent(from, new ArrayList<>());
+            outputGraph.putIfAbsent(to, new ArrayList<>());
+            outputGraph.get(from).add(to);
+            inputGraph.putIfAbsent(from, new ArrayList<>());
+            inputGraph.putIfAbsent(to, new ArrayList<>());
+            inputGraph.get(to).add(from);
+        }
+    }
+    
+    private int findGeneratedVertex() {
+        for (Map.Entry<Integer, List<Integer>> entry : inputGraph.entrySet()) {
+
+            if (entry.getValue().size() == 0) { // 생성된 정점 -> input 간선 존재 X
+                if (outputGraph.get(entry.getKey()).size() > 1) {
+                    return entry.getKey();
+                }
+            }
+        }
+        return -1;
+    }
+    
+    private int getGraphType(int start) {
+        Queue<Integer> q = new ArrayDeque<>();
+        q.offer(start);
+        while(!q.isEmpty()) {
+            int cur = q.poll();
+            visited[cur] = true;
+            if (outputGraph.get(cur).size() == 2) { // output이 2인 노드 존재 -> 8자 그래프
+                return 3;
+            }
+            for (int next : outputGraph.get(cur)) {
+                if (visited[next]) {
+                    if (next == start) {    // 돌고 돌아 처음으로 -> 도넛 그래프
+                        return 1;
+                    }
+                    continue;
+                }
+                q.offer(next);
+            }
+        }
+        // 비순환 그래프 -> 막대 그래프
+        return 2;
     }
 }
