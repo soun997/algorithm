a9d66d81522e0283094f3f7a978e118138bbec14
소윤파크 <soun997@gmail.com>
2025-08-07 02:05:17 +0900

    [level 2] Title: [1차] 캐시, Time: 25.19 ms, Memory: 134 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -2,22 +2,35 @@ import java.util.*;
 
 class Solution {
     public int solution(int cacheSize, String[] cities) {
+        // cacheSize가 0인 경우 모든 요청이 cache miss
+        if (cacheSize == 0) {
+            return cities.length * 5;
+        }
+        
         int execTime = 0;
-        List<String> cache = new LinkedList<>();
-        for (int i = 0; i < cities.length; i++) {
-            String city = cities[i].toUpperCase();
-            if (cache.contains(city)) {
-                cache.remove(city);
-                cache.add(city);
-                execTime += 1;
-                continue;
+        
+        // LinkedHashMap with access-order = true for LRU behavior
+        LinkedHashMap<String, Boolean> cache = new LinkedHashMap<String, Boolean>(16, 0.75f, true) {
+            @Override
+            protected boolean removeEldestEntry(Map.Entry<String, Boolean> eldest) {
+                return size() > cacheSize;
             }
-            cache.add(city);
-            execTime += 5;
-            if (cache.size() > cacheSize) {
-                cache.remove(0);
+        };
+        
+        for (String city : cities) {
+            String upperCity = city.toUpperCase();
+            
+            if (cache.containsKey(upperCity)) {
+                // Cache hit: get을 호출하여 access order 업데이트
+                cache.get(upperCity);
+                execTime += 1;
+            } else {
+                // Cache miss: 새로운 항목 추가
+                cache.put(upperCity, true);
+                execTime += 5;
             }
         }
+        
         return execTime;
     }
 }
