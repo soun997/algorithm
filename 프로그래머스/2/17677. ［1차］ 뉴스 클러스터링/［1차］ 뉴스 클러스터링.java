ca8296ef6bb79be2299f9609993de53095cdfe05
소윤파크 <soun997@gmail.com>
2025-08-10 06:10:53 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 12.58 ms, Memory: 90.2 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -2,48 +2,56 @@ import java.util.*;
 
 class Solution {
     
-    Map<String, Integer> map1 = new HashMap<>();
-    Map<String, Integer> map2 = new HashMap<>();
+    private final int CONST = 65536;
     
     public int solution(String str1, String str2) {
-        int answer = 0;
+        Map<String, Integer> a = parse(str1);
+        Map<String, Integer> b = parse(str2);
         
-        makeSubset(map1, str1);
-        makeSubset(map2, str2);
-
-        int intersection = 0;
-        int union = 0;
-        for (String key : map1.keySet()) {
-            if (map2.containsKey(key)) {
-                intersection += Math.min(map1.get(key), map2.get(key));
-                union += Math.max(map1.get(key), map2.get(key));
-                map2.remove(key);
-                continue;
-            }
-            union += map1.get(key);
-        }
-        for (Integer value : map2.values()) {
-            union += value;
-        }
+        int intersection = getIntersectionSize(a, b);
+        int union = getUnionSize(a, b);
         
-        if (union == 0) {
-            return 65536;
+        if (intersection == 0 && union == 0) {
+            return 1 * CONST;
         }
-        return (int) ((double) intersection / union * 65536);
+        return (int)((float)intersection / union * CONST);
     }
     
-    private void makeSubset(Map<String, Integer> map, String str) {
+    private Map<String, Integer> parse(String str) {
+        Map<String, Integer> map = new HashMap<>();
         for (int i = 0; i < str.length() - 1; i++) {
-            String part = str.substring(i, i + 2).toLowerCase();
-            if (!isLetter(part)) {
-                continue;
+            String sub = str.substring(i, i + 2).toUpperCase();
+            if (sub.matches("^[A-Za-z]*$")) {   // 영어로만 이루어져 있다면
+                map.putIfAbsent(sub, 0);
+                map.computeIfPresent(sub, (k, v) -> v + 1);
             }
-            map.merge(part, 1, Integer::sum);
         }
+        return map;
     }
     
-    private boolean isLetter(String part) {
-        return Character.isAlphabetic(part.charAt(0)) && 
-            Character.isAlphabetic(part.charAt(1));
+    private int getIntersectionSize(Map<String, Integer> a, Map<String, Integer> b) {
+        int size = 0;
+        for(String sub : a.keySet()) {
+            if (b.containsKey(sub)) {
+                size += Math.min(a.get(sub), b.get(sub));
+            }
+        }
+        return size;
+    }
+    
+    private int getUnionSize(Map<String, Integer> a, Map<String, Integer> b) {
+        int size = 0;
+        for (String sub : a.keySet()) {
+            if (b.containsKey(sub)) {
+                size += Math.max(a.get(sub), b.get(sub));
+                b.remove(sub);
+                continue;
+            }
+            size += a.get(sub);
+        }
+        for (String sub : b.keySet()) {
+            size += b.get(sub);
+        }
+        return size;
     }
 }
