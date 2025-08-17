9b08ae8da7e38617f938455ff77e3dc4b6a57c75
소윤파크 <soun997@gmail.com>
2025-08-17 10:14:03 +0900

    [level 3] Title: 이중우선순위큐, Time: 47.25 ms, Memory: 122 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -1,40 +1,26 @@
 import java.util.*;
-import java.util.function.*;
 
 class Solution {
-    
-    private Map<String, Consumer<Integer>> commands = Map.of(
-        "I", this::insert,
-        "D", this::delete
-    );
-    private List<Integer> pq = new ArrayList<>();
-    
     public int[] solution(String[] operations) {
-        for (String op : operations) {
-            String[] chunks = op.split(" ");
-            String command = chunks[0];
-            int number = Integer.parseInt(chunks[1]);
-            commands.get(command).accept(number);
+        Queue<Integer> minpq = new PriorityQueue<>();
+        Queue<Integer> maxpq = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (String operation : operations) {
+            if (operation.startsWith("I ")) {
+                int n = Integer.parseInt(operation.substring(2));
+                minpq.offer(n);
+                maxpq.offer(n);
+            } else if (!minpq.isEmpty() && operation.equals("D -1")) {
+                maxpq.remove(minpq.poll());
+            } else if (!maxpq.isEmpty() && operation.equals("D 1")) {
+                minpq.remove(maxpq.poll());
+            }
         }
-        if (pq.isEmpty()) {
+
+        if (minpq.isEmpty() && maxpq.isEmpty()) {
             return new int[]{0, 0};
         }
-        return new int[]{pq.get(pq.size() - 1), pq.get(0)};
+
+        return new int[]{maxpq.poll(), minpq.poll()};
     }
-    
-    private void insert(int number) {
-        pq.add(number);
-        Collections.sort(pq);
-    }
-    
-    private void delete(int option) {
-        if (pq.isEmpty()) {
-            return;
-        }
-        if (option == 1) {
-            pq.remove(pq.size() - 1);
-            return;
-        }
-        pq.remove(0);
-    } 
 }
