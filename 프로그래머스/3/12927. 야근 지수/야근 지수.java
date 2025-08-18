c538d983fd18a464c6f18ed8d34843ca6621a3e2
소윤파크 <soun997@gmail.com>
2025-08-18 09:00:43 +0900

    [level 3] Title: 야근 지수, Time: 134.22 ms, Memory: 70.9 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.java
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -2,20 +2,19 @@ import java.util.*;
 
 class Solution {
     public long solution(int n, int[] works) {
-        
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        for (int work : works) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>((x, y) -> Integer.compare(x, y) * -1);
+        for (int work: works) {
             pq.offer(work);
         }
-        while(n > 0) {
-            int cur = pq.poll();
-            cur = Math.max(0, cur - 1);
-            pq.offer(cur);
+        while (!pq.isEmpty() && n > 0) {
+            int remain = Math.max(0, pq.poll() - 1);
             n--;
+            pq.offer(remain);   
         }
         long answer = 0;
-        for (int work: pq) {
-            answer += (long) work * work;
+        while(!pq.isEmpty()) {
+            int fatigue = pq.poll();
+            answer += fatigue * fatigue;
         }
         return answer;
     }
