0dbd6b33e5e665442ed279cda817693dd3d32411
소윤파크 <soun997@gmail.com>
2025-08-19 09:31:02 +0900

    [level 2] Title: [3차] 압축, Time: 3.68 ms, Memory: 85.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> solution(String msg) {
+        List<Integer> answer = new ArrayList<>();
+        Map<String, Integer> dict = new HashMap<>();
+        for (char i = 'A'; i <= 'Z'; i++) {
+            dict.put(Character.toString(i), dict.size() + 1);
+        }
+        String w = "";
+        for (int i = 0; i < msg.length(); i++) {
+            w = msg.substring(i, i + 1);
+            while (i + 1 < msg.length()) {
+                String c = msg.substring(i + 1, i + 2);
+                String wc = w + c;
+                if (dict.containsKey(wc)) {
+                    w += c;
+                    i++;
+                } else {
+                    answer.add(dict.get(w));
+                    dict.put(wc, dict.size() + 1);
+                    break;
+                }
+            }
+        }
+        if (dict.containsKey(w)) {
+            answer.add(dict.get(w));
+        }
+        return answer;
+    }
+}
