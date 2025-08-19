b8fc422109f9df4520852540dc35e6099bbb695b
소윤파크 <soun997@gmail.com>
2025-08-19 10:23:03 +0900

    [level 2] Title: 주식가격, Time: 16.08 ms, Memory: 83.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,24 +1,14 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int[] prices) {
-        Stack<Integer> stk = new Stack<>();
-        int[] answer = new int[prices.length];
-        for (int i = 0; i < prices.length; i++) {
-            // 스택이 비어있거나 주가 떨어지지 않음
-            if (stk.isEmpty() || prices[stk.peek()] <= prices[i]) {
-                stk.push(i);
-                continue;
+        int len = prices.length;
+        int[] answer = new int[len];
+        int i, j;
+        for (i = 0; i < len; i++) {
+            for (j = i + 1; j < len; j++) {
+                answer[i]++;
+                if (prices[i] > prices[j])
+                    break;
             }
-            // 주가 떨어짐
-            while (!stk.isEmpty() && prices[stk.peek()] > prices[i]) {
-                int idx = stk.pop();
-                answer[idx] = i - idx;   
-            }                
-            stk.push(i);
-        }
-        for (int idx: stk) {
-            answer[idx] = (prices.length - 1) - idx;
         }
         return answer;
     }
