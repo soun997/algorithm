acd19a65e0394e3ba3214a835d8a2f6cdd1f21ed
소윤파크 <soun997@gmail.com>
2025-08-20 07:50:35 +0900

    [level 3] Title: 단어 변환, Time: 0.14 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,51 +1,56 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 class Solution {
-    
-    private static final int FAILED = Integer.MAX_VALUE;
-    private String target;
-    private String[] words;
-    private boolean[] visited;
-    private int answer = FAILED;
-    
+
+    static class Node {
+        String next;
+        int edge;
+
+        public Node(String next, int edge) {
+            this.next = next;
+            this.edge = edge;
+        }
+    }
+
     public int solution(String begin, String target, String[] words) {
-        this.target = target;
-        this.words = words;
-        visited = new boolean[words.length];
-        
-        dfs(begin, 0);
-    
-        if (answer == FAILED) {
-            return 0;
-        }
-        return answer;
-    }
-    
-    private void dfs(String word, int count) {
-        if (word.equals(target)) {
-            answer = Math.min(answer, count);
-            return;
-        }
-        for (int i = 0; i < words.length; i++) {
-            if (!visited[i] && isConvertable(word, words[i])) {
-                visited[i] = true;
-                dfs(words[i], count + 1);
-                visited[i] = false;
+        int n = words.length, ans = 0;
+
+        // for (int i=0; i<n; i++)
+        //  if (words[i] != target && i == n-1) return 0;
+
+        Queue<Node> q = new LinkedList<>();
+
+
+        boolean[] visit = new boolean[n];
+        q.add(new Node(begin, 0));
+
+        while(!q.isEmpty()) {
+            Node cur = q.poll();
+            if (cur.next.equals(target)) {
+                ans = cur.edge;
+                break;
+            }
+
+            for (int i=0; i<n; i++) {
+                if (!visit[i] && isNext(cur.next, words[i])) {
+                    visit[i] = true;
+                    q.add(new Node(words[i], cur.edge + 1));
+                }
             }
         }
+
+        return ans;
     }
-    
-    private boolean isConvertable(String from, String to) {
-        int diff = 0;
-        for (int i = 0; i < from.length(); i++) {
-            if (from.charAt(i) != to.charAt(i)) {
-                diff++;
-            }
-            if (diff > 1) {
-                return false;
+
+    static boolean isNext(String cur, String n) {
+        int cnt = 0;
+        for (int i=0; i<n.length(); i++) {
+            if (cur.charAt(i) != n.charAt(i)) {
+                if (++ cnt > 1) return false;
             }
         }
-        if (diff == 1) {
-            return true;
-        }
-        return false;
-    }
+
+        return true;
+    }    
 }
