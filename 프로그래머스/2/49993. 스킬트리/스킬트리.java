23d8f4627d293ab0c14e2a9183a3c2fb49549e2e
소윤파크 <soun997@gmail.com>
2025-08-21 09:55:02 +0900

    [level 2] Title: 스킬트리, Time: 0.45 ms, Memory: 84 MB -BaekjoonHub

---
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int solution(String skill, String[] skillTrees) {
+        int answer = 0;
+        for (String skillTree : skillTrees) {
+            if (isPossible(skill, skillTree)) {
+                answer++;
+            }
+            System.out.println();
+        }
+        return answer;
+    }
+    
+    private boolean isPossible(String skill, String skillTree) {
+        int[] indexes = new int[skill.length()];
+        for (int i = 0; i < skill.length(); i++) {
+            int index = skillTree.indexOf(skill.charAt(i));
+            if (index == -1) {
+                indexes[i] = Integer.MAX_VALUE;
+                continue;
+            }
+            indexes[i] = index;
+        } 
+        int prev = -1;
+        for (int cur: indexes) {
+            if (prev > cur) {
+                return false;
+            }
+            prev = cur;
+        }
+        return true;
+    }
+}
