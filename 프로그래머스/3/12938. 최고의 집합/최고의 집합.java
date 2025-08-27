ddbb6765d5cc013fb720737cd07589ddac050362
소윤파크 <soun997@gmail.com>
2025-08-27 11:05:37 +0900

    [level 3] Title: 최고의 집합, Time: 0.27 ms, Memory: 55.2 MB -BaekjoonHub

--- 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
@@ -1,22 +1,19 @@
 import java.util.*;
 
 class Solution {
-    
     public int[] solution(int n, int s) {
-        
-        if (s / n == 0) {
-            return new int[]{-1};
-        }
         int[] answer = new int[n];
-        int quotient = s / n;
-        int remain  = s % n;
-        for (int i = 0; i < n - remain; i++) {
-            answer[i] = quotient;
+        int idx = 0;
+        while (n > 1) {
+            int k = s / n;
+            if (k == 0) {
+                return new int[] { -1 };
+            }
+            answer[idx++] = k;
+            s -= k;
+            n -= 1;
         }
-        for (int i = 0; i < remain; i++) {
-            answer[n - remain + i] = quotient + 1;
-        }
-        
+        answer[idx] = s;
         return answer;
     }
 }
