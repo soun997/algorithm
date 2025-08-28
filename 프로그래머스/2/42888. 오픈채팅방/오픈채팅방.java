66e4036c2707de9ad98096b5647afe1c724bf353
소윤파크 <soun997@gmail.com>
2025-08-28 14:23:16 +0900

    [level 2] Title: 오픈채팅방, Time: 616.90 ms, Memory: 362 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -2,15 +2,14 @@ import java.util.*;
 
 class Solution {
     
-    private static final String ENTER_TEMPLATE = "%s님이 들어왔습니다.";
-    private static final String LEAVE_TEMPLATE = "%s님이 나갔습니다.";
+    private static final String ENTER_FORMAT = "%s님이 들어왔습니다.";
+    private static final String LEAVE_FORMAT = "%s님이 나갔습니다.";
 
     public List<String> solution(String[] records) {
         Map<String, String> nicknames = new HashMap<>();
         for (String record : records) {
             String[] chunks = record.split(" ");
-            String command = chunks[0];
-            if (command.equals("Enter") || command.equals("Change")) {
+            if (chunks.length > 2) {
                 nicknames.put(chunks[1], chunks[2]); // id, nickname   
             }
         }
@@ -20,10 +19,10 @@ class Solution {
             String command = chunks[0];
             String nickname = nicknames.get(chunks[1]);
             if (command.equals("Enter")) {
-                answer.add(String.format(ENTER_TEMPLATE, nickname));
+                answer.add(String.format(ENTER_FORMAT, nickname));
             }
             if (command.equals("Leave")) {
-                answer.add(String.format(LEAVE_TEMPLATE, nickname));
+                answer.add(String.format(LEAVE_FORMAT, nickname));
             }
         }
         return answer;
