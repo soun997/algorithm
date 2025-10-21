b8adab3d3e620ce2f6a91d86c9c10f3478d2ccb7
소윤파크 <soun997@gmail.com>
2025-10-21 10:39:13 +0900

    [level 3] Title: 풍선 터트리기, Time: 41.13 ms, Memory: 148 MB -BaekjoonHub

--- 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.java
+++ 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.java
@@ -2,21 +2,21 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] a) {
-        int[] dp1 = new int[a.length];
-        int[] dp2 = new int[a.length];
-        dp1[0] = a[0];
-        dp2[a.length - 1] = a[a.length - 1];
+        int[] left = new int[a.length];
+        int[] right = new int[a.length];
+        left[0] = a[0];
+        right[a.length - 1] = a[a.length - 1];
         for (int i = 1; i < a.length; i++) {
-            dp1[i] = Math.min(dp1[i - 1], a[i]);
-            dp2[a.length - 1 - i] = Math.min(dp2[a.length - 1 - i + 1], a[a.length - 1 - i]);
+            left[i] = Math.min(left[i - 1], a[i]);
+            right[a.length - 1 - i] = Math.min(right[a.length - 1 - i + 1], a[a.length - 1 - i]);
         }
         int answer = 0;
         for (int i = 0; i < a.length; i++) {
             int count = 0;
-            if (i - 1 >= 0 && a[i] > dp1[i - 1]) {  // 왼쪽 살피기
+            if (i - 1 >= 0 && a[i] > left[i - 1]) {  // 왼쪽 살피기
                 count++;
             }
-            if (i + 1 < a.length && a[i] > dp2[i + 1]) { // 오른쪽 살피기
+            if (i + 1 < a.length && a[i] > right[i + 1]) { // 오른쪽 살피기
                 count++;
             }
             if (count < 2) {
