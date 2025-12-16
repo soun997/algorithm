eb174cd12a979bb6ac35c7f1c3fc8d944be59314
박소윤 <soun997@gmail.com>
2025-12-16 21:54:25 +0900

    [level 3] Title: 억억단을 외우자, Time: 294.06 ms, Memory: 159 MB -BaekjoonHub

--- 프로그래머스/3/138475. 억억단을 외우자/억억단을 외우자.java
+++ 프로그래머스/3/138475. 억억단을 외우자/억억단을 외우자.java
@@ -3,22 +3,17 @@ import java.util.*;
 class Solution {
     public int[] solution(int e, int[] starts) {
         int[] counts = new int[e + 1];
-        counts[1] = 1;
-        for (int i = 2; i <= e; i++) {
-            counts[i] += 2;
-        }
-        for (int i = 2; i <= e; i++) {
-            for (int j = 2; i * j <= e; j++) {
+        for (int i = 1; i <= e; i++) {
+            for (int j = 1; i * j <= e; j++) {
                 counts[i * j]++;
             }
         }
         int[] dp = new int[e + 1];  // dp[i] = 최솟값이 i일 때 가장 많이 등장한 수
-        int max = -1;
-        for (int k = e; k >= 1; k--) {  // 거꾸로 탐색
+        dp[e] = e;
+        for (int k = e - 1; k >= 1; k--) {  // 거꾸로 탐색
             // 해당 숫자 등장 횟수가 역대 최댓값보다 더 크거나 같다 -> 갱신
-            if (counts[k] >= max) {
+            if (counts[k] >= counts[dp[k + 1]]) {
                 dp[k] = k;
-                max = counts[k];
             } else {
                 dp[k] = dp[k + 1];   
             }
