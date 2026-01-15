f45d1fbbd1daabfb5470e5dd12eeb0ebde6bb3c4
박소윤 <soun997@gmail.com>
2026-01-15 23:15:43 +0900

    [level 3] Title: 매칭 점수, Time: 12.70 ms, Memory: 86.6 MB -BaekjoonHub

--- 프로그래머스/3/42893. 매칭 점수/매칭 점수.java
+++ 프로그래머스/3/42893. 매칭 점수/매칭 점수.java
@@ -1,97 +1,86 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-
+import java.util.*;
 
 class Solution {
-
-  
     public int solution(String word, String[] pages) {
-        int answer = 0;
+        Map<String, List<String>> map = new HashMap<>();
+        Map<String, Integer> linkIndexes = new HashMap<>();
+        int[] basicScores = new int[pages.length];
+        int[] externalLinks = new int[pages.length];
+        float[] links = new float[pages.length];
         word = word.toLowerCase();
-        Matcher mt=null;
-
-
-        PageInfo[] pageInfos = new PageInfo[pages.length];
-        List<String>[] datas = new List[pages.length];
-
-		//각페이지에 관한 정보 정리
-        for(int i = 0 ; i < pages.length ; i++){
-            int score = 0 ;
-            pages[i] =pages[i].toLowerCase();
-            datas[i] = new ArrayList<>();
-            pageInfos[i] = new PageInfo();
+        for (int i = 0; i < pages.length; i++) {
+            // System.out.println(pages[i]);
+            // 링크 찾기
+            int linkStartIdx = pages[i].indexOf("<meta property=\"og:url\" content=\"") + 
+                "<meta property=\"og:url\" content=\"".length();
+            int linkEndIdx = pages[i].indexOf("\"", linkStartIdx);
+            String link = pages[i].substring(linkStartIdx, linkEndIdx);
+            map.put(link, new ArrayList<>());
+            linkIndexes.put(link, i);
+            // System.out.println("link: " + link);
             
-            //본 페이지 url를 구하는 코드
-            mt= Pattern.compile("(<meta property=\"og:url\" content=\"https://(\\S*)\")").matcher(pages[i]);
-            while(mt.find()){
-                String name = mt.group(2).trim();
-                pageInfos[i].name =name;
+            // 본문 찾기
+            int bodyStartIdx = pages[i].indexOf("<body>") + "<body>".length();
+            int bodyEndIdx = pages[i].indexOf("</body>", bodyStartIdx);
+            String body = pages[i].substring(bodyStartIdx, bodyEndIdx);
+            // System.out.println(body);
+            // 링크 찾기
+            while (body.contains("<a href")) {
+                int hrefStartIdx = body.indexOf("<a href=\"") + "<a href=\"".length();
+                int hrefEndIdx = body.indexOf("\"", hrefStartIdx);
+                String href = body.substring(hrefStartIdx, hrefEndIdx);
+                map.get(link).add(href);
+                // System.out.println("href: " + href);
+                int aStartIdx = body.indexOf("<a href=\"");
+                int aEndIdx = body.indexOf("</a>", aStartIdx) + "</a>".length();
+                body = body.substring(0, aStartIdx) + body.substring(aEndIdx, body.length());
+                externalLinks[i]++;
             }
-
-			//본 페이지 basicScore를 구하는 코드
-            mt = Pattern.compile("(?<=[^a-zA-Z])("+word+")[^a-zA-Z]").matcher(pages[i]);
-            while(mt.find()){
-                score +=1;
-            }
-            pageInfos[i].basicScore = score;
-
-
-			////본 페이지 외부 url를 구하는 코드
-            mt = Pattern.compile("<a href=\"(\\S*)//(\\S*)\"").matcher(pages[i]);
-            while(mt.find()){
-                String url = mt.group(2).trim();
-                datas[i].add(url);
-            }
-            pageInfos[i].linkedOutPage = datas[i];
-            pageInfos[i].outerCnt = datas[i].size();
-
-        }
-
-
-
-		//외부 링크를 계산하여 총점을 구하는 코드 
-        for(int i = 0 ; i < pageInfos.length;i++){
-
-            for(String url : pageInfos[i].linkedOutPage){
-                for(int k = 0 ; k < pageInfos.length;k++){
-                    if(i == k) continue;
-                    if(url.equals(pageInfos[k].name)){
-                        pageInfos[k].score += (double)pageInfos[i].basicScore /pageInfos[i].linkedOutPage.size();
+            // System.out.println(body);
+            // System.out.println();
+            
+            body = body.toLowerCase();
+            for (int j = 0; j <= body.length() - word.length(); j++) {
+                String sub = body.substring(j, j + word.length());
+                if (sub.equals(word)) {
+                    boolean front = (j == 0 || !Character.isLetter(body.charAt(j - 1)));
+                    boolean back = (j + word.length() >= body.length() || 
+                                     !Character.isLetter(body.charAt(j + word.length())));
+                    if (front && back) {
+                        basicScores[i]++;
                     }
                 }
             }
         }
-
-		//최대 스코어의 index를 구하는 코드 
         
-        double max = 0;
-		for(int i = 0 ; i < pageInfos.length;i++) {
-            if(max < (pageInfos[i].basicScore+pageInfos[i].score)){
-                max = (pageInfos[i].basicScore+pageInfos[i].score);
-                answer = i;
+        for (String myLink : map.keySet()) {
+            // System.out.println("myLink: " + myLink);
+            for (String externalLink : map.keySet()) {
+                if (myLink.equals(externalLink)) {
+                    continue;
+                }
+                for (String link : map.get(externalLink)) {
+                    if (myLink.equals(link)) {
+                        int myLinkIndex = linkIndexes.get(myLink);
+                        int externalLinkIndex = linkIndexes.get(externalLink);
+                        // System.out.println("externalLink: " + externalLink);
+                        if (externalLinks[externalLinkIndex] > 0) {
+                            links[myLinkIndex] += 
+                                (float)(basicScores[externalLinkIndex]) / externalLinks[externalLinkIndex];   
+                        }
+                    }
+                }
             }
         }
-
-
+        int answer = 0;
+        float max = basicScores[0] + links[0];
+        for (int i = 0; i < pages.length; i++) {
+            float matchingScore = basicScores[i] + links[i];
+            if (max < matchingScore) {
+                answer = i;
+                max = matchingScore;
+            }
+        }
         return answer;
-    }
-
-	
-    //각 페이지가 가지고 있는 정보
-    class PageInfo{
-    	//본 페이지 URL
-        String name;
-        //본 페이지가 가지고 있는 외부 URL 갯수
-        int outerCnt;
-        //본 페이지가 가지고 있는 기본 Score
-        int basicScore;
-        //본 페이지가 가지고 있는 외부 URL 
-        List<String> linkedOutPage;
-        //기본 Score + 외부 url 계산 score
-        double score;
-
     }
 }
